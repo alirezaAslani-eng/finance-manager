f317feec977972bb1f8fdf7a3378e8927e47de02
Alireza Aslani <alirezaaslane.mob7@gmail.com>
2025-08-19 19:25:36 +0330

    fix(rule)

--- lib/validations/rules/accountRule.ts
+++ lib/validations/rules/accountRule.ts
@@ -2,9 +2,9 @@ import { Account_face } from "@/types/account.types";
 import { schema_model } from "@/types/utils";
 
 const accountRule: schema_model<Account_face> = {
-  cardNumber: { type: String, required: true, match: /^[0-9]{16}$/ },
+  cardNumber: { type: String, required: true, match: /^[0-9]{16}$/},
   currentBalance: { type: String, required: true, match: /^[0-9]+$/ },
-  createdAt: { type: Date, maxlength: 10, required: true },
+  createdAt: { type: Date, required: true },
 };
 
 export default accountRule;
