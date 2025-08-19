c598bfa35f1bf7b4867b478e9da1755b96426ff1
Alireza Aslani <alirezaaslane.mob7@gmail.com>
2025-08-19 20:34:09 +0330

    feat(API) create POST

--- pages/api/transactions/index.ts
+++ pages/api/transactions/index.ts
@@ -1,5 +1,34 @@
+import { transactionServices } from "@/lib/services";
 import { apiHandler } from "@/lib/utils";
+import { transactionSchema } from "@/lib/validations";
 import { handler_type } from "@/types/api.types";
-const handler: handler_type = async (req, res) => {};
+import { Transaction_face } from "@/types/transaction.types";
+import { ApiError } from "next/dist/server/api-utils";
+const handler: handler_type = async (req, res) => {
+  // * Services =============== >
+  const { createTransaction, removeTransaction } = transactionServices;
+  // * Body from client ================= >
+  const body = req.body as Pick<Transaction_face, "amount" | "reason" | "type">;
+  // * Zod validator ========================= >
+  const { amount, reason } = transactionSchema.parse(body);
+
+  switch (req.method as "POST" | "DELETE") {
+    case "POST": {
+      const create_res = await createTransaction({
+        // from client ---- >
+        type: body?.type, // no zod validation
+        amount,
+        reason,
+        // api side - >
+        accountBalance: "40000",
+        createdAt: new Date().getUTCFullYear(),
+      });
+      return res.json(create_res);
+    }
+    default: {
+      throw new ApiError(400, "request method is not valid");
+    }
+  }
+};
 
 export default apiHandler(handler);
