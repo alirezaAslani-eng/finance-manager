bb61afa6e5ded0e84de1d5aaaf9643b355a24cb8
Alireza Aslani <alirezaaslane.mob7@gmail.com>
2025-08-19 19:47:27 +0330

    feat(transaction) add clinet schema

--- lib/validations/transactionSchema.ts
+++ lib/validations/transactionSchema.ts
@@ -1,5 +1,19 @@
+import { Transaction_face } from "@/types/transaction.types";
+import { Test_type } from "@/types/utils";
 import z from "zod";
 
-const transactionSchema = z.object({});
+const transactionSchema = z.object({
+  amount: z
+    .string("لطفا فیلد را پر کنید")
+    .regex(/^[0-9]+$/, "مقدار معتبر نیست"),
+  reason: z
+    .string("لطفا فیلد را پر کنید")
+    .min(3, "حد اقل 3 حرف")
+    .max(500, "حد اکثر 500 حرف"),
+});
 
-export default transactionSchema
+type Test_schema = Test_type<
+  z.infer<typeof transactionSchema>,
+  Pick<Transaction_face, "amount" | "reason">
+>;
+export default transactionSchema;
