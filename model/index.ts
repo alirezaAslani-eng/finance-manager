363d6139327e8c1c5358fca0b22c5e08d1665b64
Alireza Aslani <alirezaaslane.mob7@gmail.com>
2025-08-19 16:24:01 +0330

    fix(export) export name

--- model/index.ts
+++ model/index.ts
@@ -1,3 +1,3 @@
-export { default as user } from "./user";
-export { default as account } from "./account";
-export { default as transaction } from "./transaction";
+export { default as user_model } from "./user";
+export { default as account_model } from "./account";
+export { default as transaction_model } from "./transaction";
