a72e01d098457156e57b0524293403fe2fff4416
Alireza Aslani <alirezaaslane.mob7@gmail.com>
2025-08-21 14:46:36 +0330

    feat(add) timestamp

--- model/user.ts
+++ model/user.ts
@@ -2,7 +2,7 @@ import { userRule } from "@/lib/validations/rules";
 import { User_face } from "@/types/user.types";
 import m, { model, models, Schema } from "mongoose";
 
-const schema = new Schema(userRule);
+const schema = new Schema(userRule, { timestamps: true });
 
 const user_model = models.User || model<User_face>("User", schema);
 
