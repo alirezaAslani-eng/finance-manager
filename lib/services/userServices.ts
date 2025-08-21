466dc8616095c694405d21e15abaaf793fe5124f
Alireza Aslani <alirezaaslane.mob7@gmail.com>
2025-08-21 15:51:36 +0330

    fix(service) user register method must be take the same value as it's rule

--- lib/services/userServices.ts
+++ lib/services/userServices.ts
@@ -1,9 +1,10 @@
 import { User_face } from "@/types/user.types";
 import { conect } from "../db";
 import { user_model } from "@/model";
+import { userRule } from "../validations/rules";
 
 const userServices = {
-  async registerUser(userInfo: User_face) {
+  async registerUser(userInfo: typeof userRule) {
     await conect();
     const reg_res = await user_model.create(userInfo);
     return reg_res;
