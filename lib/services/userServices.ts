1d0414956789fb14630e97d6820fcfeb9ed6203d
Alireza Aslani <alirezaaslane.mob7@gmail.com>
2025-08-21 15:09:43 +0330

    feat(user) create new user (query)

--- lib/services/userServices.ts
+++ lib/services/userServices.ts
@@ -1,8 +1,12 @@
+import { User_face } from "@/types/user.types";
 import { conect } from "../db";
+import { user_model } from "@/model";
 
 const userServices = {
-  async registerUser() {
+  async registerUser(userInfo: User_face) {
     await conect();
+    const reg_res = await user_model.create(userInfo);
+    return reg_res;
   },
   async loginUser() {
     await conect();
