40ab38b44f5b1a84ec466c61a91f61185da02717
Alireza Aslani <alirezaaslane.mob7@gmail.com>
2025-08-21 14:45:54 +0330

    feat(create) user schema by (zod)

--- lib/validations/userSchema.ts
+++ lib/validations/userSchema.ts
@@ -1,5 +1,27 @@
-import z from "zod";
+import { object, string, Infer } from "zod";
+import { User_face } from "@/types/user.types";
+const userSchema = object({
+  userName: string()
+    .min(5, "حد اقل 5 حرف")
+    .max(30, "حد اکثر 30 حرف")
+    .regex(/^[A-Za-zآ-ی0-9_.]+$/, "نام کاربری نا معتبر"),
+  fullName: string()
+    .min(5, "حد اقل 5 حرف")
+    .max(30, "حد اکثر 30 حرف")
+    .regex(/^[A-Za-zآ-ی]+$/, "نام کاربری نا معتبر"),
+  password: string()
+    .min(6)
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}$/, "رمز نا معتبر"),
+  phone: string()
+    .min(11, "شماره 11 رقم نیست")
+    .regex(/^09[0-9]{9}$/, "شماره نامعتبر هست"),
+});
 
-const userSchema = z.object({});
-
+// * Check Validation Type ================ >
+type Test = Infer<typeof userSchema> extends Pick<
+  User_face,
+  "fullName" | "password" | "phone" | "userName"
+>
+  ? true
+  : false;
 export default userSchema;
