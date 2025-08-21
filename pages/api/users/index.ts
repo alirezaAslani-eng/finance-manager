fdfd8d004d6f4daf4e27d4a4f1d09c35758849b7
Alireza Aslani <alirezaaslane.mob7@gmail.com>
2025-08-21 15:12:51 +0330

    feat(API) user registering (base logic)

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,5 +1,19 @@
 import { apiHandler } from "@/lib/utils";
 import { handler_type } from "@/types/api.types";
-const handler: handler_type = async (req, res) => {};
+import { userServices } from "@/lib/services";
+import { userSchema } from "@/lib/validations";
+const handler: handler_type = async (req, res) => {
+  // * User Services  ================== >
+  const { registerUser } = userServices;
+  switch (req.method as "POST") {
+    case "POST": {
+      // * Zod Validation ==================== >
+      const safe_user_info = userSchema.parse(req.body);
+      // * Create User =============== >
+      const reg_res = await registerUser(safe_user_info); // ! Might Throw Error
+      return res.json(reg_res); // * Response <<<
+    }
+  }
+};
 
 export default apiHandler(handler);
