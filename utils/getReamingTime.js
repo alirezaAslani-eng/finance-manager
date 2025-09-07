28236b8fd72f8f3a2d9bf73f3618a9671916a63e
Alireza <alirezaaslane.mob7@gmail.com>
2025-09-07 13:02:10 +0330

    feat(utility) a function to transform ms to a natural format that user can understan

---
+++ utils/getReamingTime.js
@@ -0,0 +1,26 @@
+/**
+ * This method needs to take a time from future as (ms) and it return a text that tells users how mush they need to wait
+ * @param {number} futureMs
+ * @returns {string}
+ */
+const getReamingTime = (futureMs) => {
+  const now = new Date().getTime();
+  const ms = futureMs - now;
+
+  let time = 0; // time containes a number can be seconds, hourse, or minutes .
+  let unit = ""; // and this variable describe the unit of time .
+
+  if (ms <= 60000) {
+    unit = "سانیه";
+    time = Math.ceil(ms / 1000);
+  } else if (ms <= 3600000) {
+    unit = "دقیقه";
+    time = Math.round(ms / 60000);
+  } else {
+    unit = "ساعت";
+    time = Math.round(ms / 3600000);
+  }
+  return `${time} ${unit}`;
+};
+
+export default getReamingTime;
---
+++ utils/index.js
@@ -0,0 +1,1 @@
+export {default as getReamingTime} from "./getReamingTime"
