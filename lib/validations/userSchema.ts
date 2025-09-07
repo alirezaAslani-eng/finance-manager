2a076fe0648577af0ffac0b8cafbdbc5b8f41b1a
Alireza <alirezaaslane.mob7@gmail.com>
2025-09-07 15:27:08 +0330

    feat(schema) body to update user's info

--- lib/validations/userSchema.ts
+++ lib/validations/userSchema.ts
@@ -5,12 +5,21 @@ const userSchema = object({
     .min(5, "حد اقل 5 حرف")
     .max(30, "حد اکثر 30 حرف")
     .regex(/^[A-Za-zآ-ی0-9_.]+$/, "نام کاربری نا معتبر"),
-  fullName: string()
-    .min(5, "حد اقل 5 حرف")
-    .max(30, "حد اکثر 30 حرف"),
+  fullName: string().min(5, "حد اقل 5 حرف").max(30, "حد اکثر 30 حرف"),
   password: string()
     .min(6, "حد اقل شامل 6 حرف")
     .regex(/^[A-Za-z0-9]+$/, "رمز عبور باید شامل حروف و اعداد انگلیسی باشد"),
+  phone: string().regex(/^09[0-9]{9}$/, "شماره نامعتبر هست"),
+  email: string()
+    .nonempty("فیلد نباید خالی باشه")
+    .regex(/^[A-Za-z0-9._%+-]+@gmail\.com$/, "ایمیل معتبر نیست"),
+});
+const editUserSchema = object({
+  userName: string()
+    .min(5, "حد اقل 5 حرف")
+    .max(30, "حد اکثر 30 حرف")
+    .regex(/^[A-Za-zآ-ی0-9_.]+$/, "نام کاربری نا معتبر"),
+  fullName: string().min(5, "حد اقل 5 حرف").max(30, "حد اکثر 30 حرف"),
   phone: string().regex(/^09[0-9]{9}$/, "شماره نامعتبر هست"),
   email: string()
     .nonempty("فیلد نباید خالی باشه")
@@ -34,4 +43,4 @@ type Test = Infer<typeof userSchema> extends Pick<
 >
   ? true
   : false;
-export { userSchema, loginSchema };
+export { userSchema, loginSchema, editUserSchema };
