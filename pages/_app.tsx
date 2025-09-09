40d8d887eca9d3dbc20c16ed97f6204810b4cd29
Alireza <alirezaaslane.mob7@gmail.com>
2025-09-09 17:20:13 +0330

    feat(font&css) reset css by CssBaseline and load medium font by next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,23 @@
-import type { AppProps } from 'next/app'
-import React from 'react'
-function _app({Component,pageProps}:AppProps) {
+import { CssBaseline } from "@mui/material";
+import type { AppProps } from "next/app";
+import React from "react";
+import loadDanaFont from "next/font/local";
+
+
+// * Load Dana Medume Font as Gloabal ================= >
+const danaMediume = loadDanaFont({
+  src: "../assets/fonts/DanaFaNum-DemiBold.woff2",
+});
+
+function _app({ Component, pageProps }: AppProps) {
   return (
-   <Component {...pageProps} />
-  )
+    <main className={danaMediume.className}>
+      {/* Normalize Css ================== >*/}
+      <CssBaseline />
+      {/* Render Page ================== >*/}
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
-export default _app
+export default _app;
