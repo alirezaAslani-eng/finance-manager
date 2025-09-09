d30a5fc024d8058b60749100d180a04b15fa055f
Alireza <alirezaaslane.mob7@gmail.com>
2025-09-09 19:02:41 +0330

    feat(init) landing module and topBar for introduction page (public page)

---
+++ components/module/Landing/Landing.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LandingTopBar from "../LandingTopBar/LandingTopBar";
+
+function Landing() {
+  return (
+    <div>
+      <LandingTopBar />
+      {/* Landing Content */}
+    </div>
+  );
+}
+
+export default Landing;
---
+++ components/module/LandingTopBar/LandingTopBar.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+function LandingTopBar() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    ></Box>
+  );
+}
+
+export default LandingTopBar;
