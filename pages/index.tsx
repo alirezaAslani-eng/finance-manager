124ae38a952a9803fcdf2bba8408a046bb2b6c0f
Alireza <alirezaaslane.mob7@gmail.com>
2025-09-10 12:57:29 +0330

    fix(page) public home page must be in this path "/" because this page introduce the website

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,15 @@
-
-
-
-export default function Home() {
-  return <></>;
+import React from "react";
+import { Footer, Landing } from "@/components/module";
+import { MuiTitleText } from "@/components/ui";
+function PublicHome() {
+  return (
+    <div>
+      <Landing />
+      <MuiTitleText text="" title="" />
+      <MuiTitleText text="" title="" />
+      <Footer />
+    </div>
+  );
 }
+
+export default PublicHome;
