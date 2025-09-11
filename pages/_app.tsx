130c596b5f5a281f1bdd60418e8a1242045ed2b9
Alireza <alirezaaslane.mob7@gmail.com>
2025-09-11 20:27:45 +0330

    feat(font) add a font for heading text

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,22 @@
-import { CssBaseline } from "@mui/material";
+import { Box, CssBaseline } from "@mui/material";
 import type { AppProps } from "next/app";
 import React from "react";
-import loadDanaFont from "next/font/local";
+import font from "next/font/local";
 import { MuiThemeProvider } from "@/context";
 
 // * Load Dana Medume Font as Gloabal ================= >
-const danaMediume = loadDanaFont({
+const danaMediume = font({
   src: "../assets/fonts/DanaFaNum-DemiBold.woff2",
   variable: "--dana-md",
+});
+const peydaMedium = font({
+  src: "../assets/fonts/PeydaWeb-Medium.woff",
+  variable: "--peyda-md",
 });
 
 function _app({ Component, pageProps }: AppProps) {
   return (
-    <main className={danaMediume.className}>
+    <main className={`${danaMediume.variable} ${peydaMedium.variable}`}>
       <MuiThemeProvider>
         {/* Normalize Css ================== >*/}
         <CssBaseline />
