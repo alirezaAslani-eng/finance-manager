e5dcdb6e237101624e7b3304bce78abe3b101b7e
Alireza <alirezaaslane.mob7@gmail.com>
2025-09-11 19:41:47 +0330

    feat(footer) a simple footer that includes developer's link is finished

--- components/module/Footer/Footer.tsx
+++ components/module/Footer/Footer.tsx
@@ -1,8 +1,25 @@
-import { Box } from "@mui/material";
+import { Box, Link, useTheme } from "@mui/material";
 import React from "react";
 
 function Footer() {
-  return <Box>Footer</Box>;
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        backgroundColor: theme.palette.primary.main,
+        width: "100%",
+        textAlign: "center",
+        fontFamily: theme.typography.h1,
+        fontSize: {xs:"18px",sm:"24px"},
+        padding: "10px",
+        color: "#fff",
+      }}
+    >
+      ساخته شده توسط :{" "}
+      <Link href="https://github.com/alirezaAslani-eng" sx={{ color: "#fff" }}>
+        Alireza Aslani
+      </Link>
+    </Box>
+  );
 }
-<div>Footer</div>
 export default Footer;
