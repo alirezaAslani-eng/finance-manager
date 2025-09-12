c5d5387b752874bbddba0e470c68b8616e9a1fdc
Alireza <alirezaaslane.mob7@gmail.com>
2025-09-12 17:35:01 +0330

    feat(TextField) add props and error section

--- components/ui/Input/MuiTextField.tsx
+++ components/ui/Input/MuiTextField.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
+import type { TextFieldProps } from "@mui/material/TextField";
+import { Box, Typography, useTheme } from "@mui/material";
 
-function MuiTextField() {
-  return <TextField id="outlined-basic" label="Outlined" variant="outlined" />;
+interface myProps {
+  textFieldProps?: TextFieldProps;
+  errorText?: string | undefined | null;
+}
+function MuiTextField({ textFieldProps = {}, errorText }: myProps) {
+  const { palette } = useTheme();
+  return (
+    <Box sx={{display:"flex",flexDirection:"column"}}>
+      <TextField
+        error={!!errorText}
+        label="Outlined"
+        variant="outlined"
+        {...textFieldProps}
+      />
+
+      {!!errorText && (
+        <Typography component={"span"} sx={{ color: palette.error.main }}>
+          {errorText}
+        </Typography>
+      )}
+    </Box>
+  );
 }
 
 export default MuiTextField;
