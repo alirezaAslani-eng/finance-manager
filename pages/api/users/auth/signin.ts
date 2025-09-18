29382676d94b213498c27cd92b26245c1e150284
Alireza <alirezaaslane.mob7@gmail.com>
2025-09-18 18:23:17 +0330

    fix(signinAPI) define what otp type will be verified

--- pages/api/users/auth/signin.ts
+++ pages/api/users/auth/signin.ts
@@ -25,7 +25,7 @@ const handler: handler_type = async (req, res) => {
   // * Body From Client ===================== >
   const { phone, otpCode } = verifySchema.parse(req.body); // ! Might Throw Error ====================== <
   // * Verify User =================================== >
-  const userInfo = await verifyOtp(phone, otpCode); // ! Might Throw Error ====================== <
+  const userInfo = await verifyOtp(phone, otpCode,{type:"signin"}); // ! Might Throw Error ====================== <
   // * Generate Token ================================ >
   const { email, fullName, _id, role } = userInfo;
   const token = generateToken({ _id, email, fullName, phone, role });
