ebe04ad9d5912c768c8edb847a875b146249009a
Alireza <alirezaaslane.mob7@gmail.com>
2025-09-19 16:21:06 +0330

    feat(component) custom error color

--- components/ui/Input/MuiTextField.tsx
+++ components/ui/Input/MuiTextField.tsx
@@ -16,10 +16,21 @@ function MuiTextField({ textFieldProps = {}, errorText }: myProps) {
         variant="outlined"
         label={textFieldProps.placeholder ?? ""}
         {...textFieldProps}
+        sx={{
+          "& .Mui-error": {
+            color: palette.error[300],
+            borderColor: palette.error[300],
+          },
+          "& .MuiOutlinedInput-root.Mui-error fieldset": {
+            borderColor: palette.error[300],
+          },
+
+          ...textFieldProps.sx,
+        }}
       />
 
       {!!errorText && (
-        <Typography component={"span"} sx={{ color: palette.error.main }}>
+        <Typography component={"span"} sx={{ color: palette.error[300] }}>
           {errorText}
         </Typography>
       )}
