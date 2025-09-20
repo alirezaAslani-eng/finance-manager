ee3d92ee261c123008dae772baf85502a3f6a319
Alireza <alirezaaslane.mob7@gmail.com>
2025-09-20 19:13:54 +0330

    feat(module) search box module initialzed

---
+++ components/module/SearchBox/TopBarSearchBox.tsx
@@ -0,0 +1,10 @@
+import { MuiTextField } from '@/components/ui'
+import React from 'react'
+
+function TopBarSearchBox() {
+  return (
+   <MuiTextField  textFieldProps={{size:"small",placeholder:"جستجو"}} /> 
+  )
+}
+
+export default TopBarSearchBox
--- components/module/index.ts
+++ components/module/index.ts
@@ -2,3 +2,4 @@ export { default as Landing } from "./Landing/Landing";
 export { default as Footer } from "./Footer/Footer";
 export { default as LoginForm} from "./Form/LoginForm";
 export { default as RegisterForm} from "./Form/RegisterForm";
+export { default as TopBarSearchBox} from "./SearchBox/TopBarSearchBox";
