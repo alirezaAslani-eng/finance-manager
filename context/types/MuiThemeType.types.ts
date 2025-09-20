75ab73b8d8e883ff09c5edadbc01e24e6c8b9247
Alireza <alirezaaslane.mob7@gmail.com>
2025-09-20 16:32:31 +0330

    feat(add theme type)

--- context/types/MuiThemeType.types.ts
+++ context/types/MuiThemeType.types.ts
@@ -1,30 +1,29 @@
 import type {
+  Palette,
   PaletteColor,
   SimplePaletteColorOptions,
+  PaletteOptions,
 } from "@mui/material/styles";
+
+interface Shades {
+  50?: string;
+  100?: string;
+  200?: string;
+  300?: string;
+  400?: string;
+  500?: string;
+  600?: string;
+  700?: string;
+  800?: string;
+  900?: string;
+}
 declare module "@mui/material/styles" {
-  interface PaletteColor {
-    50?: string;
-    100?: string;
-    200?: string;
-    300?: string;
-    400?: string;
-    500?: string;
-    600?: string;
-    700?: string;
-    800?: string;
-    900?: string;
+  interface PaletteColor extends Shades {}
+  interface SimplePaletteColorOptions extends Shades {}
+  interface Palette {
+    white: Shades;
   }
-  interface SimplePaletteColorOptions {
-    50?: string;
-    100?: string;
-    200?: string;
-    300?: string;
-    400?: string;
-    500?: string;
-    600?: string;
-    700?: string;
-    800?: string;
-    900?: string;
+  interface PaletteOptions {
+    white: Shades;
   }
 }
