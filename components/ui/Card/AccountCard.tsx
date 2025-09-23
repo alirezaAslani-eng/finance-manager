b59c5c6abf38f4321f5419e74813eff1e370247d
Alireza <alirezaaslane.mob7@gmail.com>
2025-09-23 17:58:40 +0330

    fix&feat(CardComponent) fix -> comments and style & feat -> add a button to edit account's info

--- components/ui/Card/AccountCard.tsx
+++ components/ui/Card/AccountCard.tsx
@@ -1,12 +1,21 @@
 import { MuiButton } from "@/components/ui";
 import DoneRoundedIcon from "@mui/icons-material/DoneRounded";
 import { Box, Divider, Typography, useTheme } from "@mui/material";
+import EditNoteRoundedIcon from "@mui/icons-material/EditNoteRounded";
 import React from "react";
 
 function AccountCard() {
   const theme = useTheme();
   const { palette, alpha } = theme;
 
+  const footerButton_sx = {
+    ...theme.custom.resetButton,
+    borderRadius: "999px",
+    p: "8px",
+    display: "flex",
+    alignItems: "center",
+    gap: "5px",
+  };
   return (
     <Box
       sx={{
@@ -15,9 +24,10 @@ function AccountCard() {
         overflow: "hidden",
       }}
     >
+      {/* Body Card ============================== > */}
       <Box
         sx={{
-          padding: "30px 15px",
+          padding: "20px 15px",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
@@ -33,39 +43,50 @@ function AccountCard() {
         >
           {"6037 6976 8508 5414"}
         </Typography>
+        {/* Current Balance ========================= > */}
         <Typography
           component={"span"}
           display={"block"}
           sx={{ fontSize: "20px", color: palette.grey[50] }}
         >{`مجودی : ${"20,000,000"}`}</Typography>
+        {/* Owner name =================================> */}
+        <Typography
+          component={"p"}
+          sx={{ color: palette.grey[50] }}
+        >{`به نام : ${"مریم اصلانی"}`}</Typography>
       </Box>
+
       <Divider />
+      {/* Footer =============================== > */}
       <Box
         sx={{
-          p: "15px",
+          p: "8px 10px",
           display: "flex",
           justifyContent: "space-between",
           alignItems: "center",
           backgroundColor: alpha(palette.primary.main, 0.1),
         }}
       >
+        {/* Enable Button ======================== > */}
         <MuiButton
           buttonProps={{
             variant: "outlined",
-            sx: {
-              ...theme.custom.resetButton,
-              borderRadius: "999px",
-              p: "10px",
-              display: "flex",
-              alignItems: "center",
-              gap: "10px",
-            },
+            sx: footerButton_sx,
           }}
         >
           <DoneRoundedIcon />
-          فعال کردن کارت
+          <Typography sx={{ fontSize: "12px" }}>فعال کردن کارت</Typography>
         </MuiButton>
-        <Typography component={"p"}>{`به نام : ${"مریم اصلانی"}`}</Typography>
+        {/* Edit Button ============================== > */}
+
+        <MuiButton
+          buttonProps={{
+            variant: "outlined",
+            sx: footerButton_sx,
+          }}
+        >
+          <EditNoteRoundedIcon />
+        </MuiButton>
       </Box>
     </Box>
   );
