e7ea6d536877b2f3a4363899ba8856d593032215
Alireza <alirezaaslane.mob7@gmail.com>
2025-09-28 19:50:12 +0330

    fix(architecture) remove unused component

--- components/module/index.ts
+++ components/module/index.ts
@@ -5,7 +5,6 @@ export { default as RegisterForm } from "./Form/RegisterForm";
 export { default as TransactionForm } from "./Form/TransactionForm";
 export { default as AccountForm } from "./Form/AccountForm";
 export { default as UserEditForm} from "./Form/UserEditForm";
-export { default as TopBarSearchBox } from "./SearchBox/TopBarSearchBox";
 export { default as RecentTransactions } from "./Transactions/RecentTransactions";
 export { default as Slider } from "./Slider/Slider";
 export { default as Accounts } from "./Accounts/Accounts";
