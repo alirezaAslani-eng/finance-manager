211ffc52971cd81e1bf56bb7a8eb266c4729d1fd
Alireza <alirezaaslane.mob7@gmail.com>
2025-09-30 19:43:49 +0330

    feat(responsive) fix and add sx properties & responsive this Card

--- components/ui/Card/TransactionCard.tsx
+++ components/ui/Card/TransactionCard.tsx
@@ -7,27 +7,59 @@ import ArrowOutwardRoundedIcon from "@mui/icons-material/ArrowOutwardRounded";
 import { MuiButton, TextPrice } from "@/components/ui";
 import { useTheme } from "@mui/material/styles";
 import Link from "next/link";
+import { muiTheme } from "@/utils";
 
 const TransactionCard = () => {
   const theme = useTheme();
+  const {
+    palette: { grey, mode },
+    alpha,
+  } = theme;
   return (
     <Card variant="outlined" sx={{ width: "auto", borderRadius: "20px" }}>
       <Box sx={{ p: 2 }}>
-        <Stack
-          direction="row"
-          sx={{ justifyContent: "space-between", alignItems: "center" }}
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            alignItems: "center",
+            // * Responsive Flex ===================== >
+            justifyContent: { xs: "center", md: "space-between" },
+            flexDirection: { xs: "column", md: "row" },
+          }}
         >
-          <Typography gutterBottom variant="h5" component="div">
-            خرید میوه
+          {/* // * Title ===================== > */}
+          <Typography
+            component="p"
+            // * Responsive fontSize ===================== >
+            sx={{ fontSize: { xs: "18px", sm: "22px" } }}
+          >
+            متن دسته بندی طولانی
           </Typography>
-          <TextPrice price={20_000} priceProps={{ sx: { fontSize: "26px" } }} />
-        </Stack>
-        <Typography variant="body2" sx={{ color: "text.secondary" }}>
-          امروز دو کیلو میوه برای مهمونی خریدم که اینم یک مت طولانی و تستی هست
-          برا تست این ui پنل مدیریت خرج های خونه
+          {/* // * Price ================ > */}
+          <TextPrice
+            price={20_000}
+            // * Responsive fontSize ===================== >
+            priceProps={{ sx: { fontSize: { xs: "24px", sm: "26px" } } }}
+          />
+        </Box>
+        {/* // * Description ================ > */}
+        <Typography
+          sx={{
+            // * Responsive textAlign ===================== >
+            textAlign: { xs: "center", sm: "right" },
+            color: muiTheme(mode, {
+              light: alpha(grey[900], 0.6),
+              dark: alpha(grey[50], 0.6),
+            }),
+          }}
+        >
+          امروز دو کیلو میوه برای مهمونی خریدم که اینم یک مت طولانی و برا تست
+          این ui پنل مدیریت خرج های خونه
         </Typography>
       </Box>
       <Divider />
+      {/* // * Footer Section ================= > */}
       <Box
         sx={{
           display: "flex",
@@ -56,7 +88,7 @@ const TransactionCard = () => {
           </MuiButton>
         </Link>
         {/* Date ===================== > */}
-        <Typography>22:18----1404/12/01</Typography>
+        <Typography>1404/12/01</Typography>
       </Box>
     </Card>
   );
