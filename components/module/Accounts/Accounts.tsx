13c30c55539ab9a3a65c3c426388a4097f56443c
Alireza <alirezaaslane.mob7@gmail.com>
2025-09-30 19:18:31 +0330

    feat(responsive) account slider for (mobile | tablet | desktop)

--- components/module/Accounts/Accounts.tsx
+++ components/module/Accounts/Accounts.tsx
@@ -11,18 +11,50 @@ import "swiper/css";
 import "swiper/css/navigation";
 // import required modules
 import Link from "next/link";
-import { usePaginationArray } from "@/hooks";
+import { useBreakePoints, usePaginationArray } from "@/hooks";
 
-const data = [1, 8, 8, 8, 6, 8, 8, 8]; // ! Fake data <<<<<<<<<<<
-// TODO style => Responsive 
-// TODO logic => get dynamic data 
+const data = [1, 8, 8, 8, 6, 8, 8, 8];
+// TODO logic => get dynamic data
 function DesktopCards() {
   const theme = useTheme();
   const { pagedData } = usePaginationArray(data);
+  // * Responsive Hook ==================== >
+  const { isTablet } = useBreakePoints({ decrease: 200 });
+
+  // *  After Mobile breake point it renders a grid structure  =============== >
+  const DesktopContent = pagedData.map((item) => {
+    return (
+      <SwiperSlide>
+        <Box
+          display={"grid"}
+          sx={{
+            gridTemplateColumns: "repeat(2,1fr)",
+            mt: "20px",
+            gap: "20px",
+          }}
+        >
+          {item.map((slide) => {
+            return <AccountCard />;
+          })}
+        </Box>
+      </SwiperSlide>
+    );
+  });
+  // *  Before Desktop breake point it renders a simple list of accounts  =============== >
+  const MobileContent = data.map((item) => {
+    return (
+      <SwiperSlide>
+        <Box display={"flex"} justifyContent={"center"}>
+          <AccountCard />
+        </Box>
+      </SwiperSlide>
+    );
+  });
 
   return (
     <BoxWithTitle
       title="حساب های من"
+      boxProps={{ sx: { backgroundColor: "transparent" } }}
       Button={
         <Link href="">
           <MuiButton
@@ -42,31 +74,24 @@ function DesktopCards() {
         </Link>
       }
     >
-      <Slider NavButton={NavigationButton}>
-        {pagedData.map((item) => {
-          return (
-            <SwiperSlide>
-              <Box
-                display={"grid"}
-                sx={{
-                  gridTemplateColumns: "repeat(2,1fr)",
-                  mt: "20px",
-                  gap: "20px",
-                }}
-              >
-                {item.map((slide) => {
-                  return <AccountCard />;
-                })}
-              </Box>
-            </SwiperSlide>
-          );
-        })}
-        {/* <NavButton /> */}
-      </Slider>
+      {/* //* Slider Container ===================== > */}
+      <Box
+        sx={{
+          maxWidth: {
+            xs: "400px",
+            _700: "100%",
+          },
+          mx: "auto",
+          mt: "20px",
+        }}
+      >
+        <Slider NavButton={NavigationButton}>
+          {isTablet ? DesktopContent : MobileContent}
+          {/* <NavButton /> */}
+        </Slider>
+      </Box>
     </BoxWithTitle>
   );
 }
 
 export default DesktopCards;
-
-
