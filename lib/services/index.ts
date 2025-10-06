a40c0bc3a359ac8eac881fba819d451873d2461e
Alireza <alirezaaslane.mob7@gmail.com>
2025-10-06 21:08:20 +0330

    feat(services) create a query runer that resposible for initialize user it creates an account and a category

--- lib/services/index.ts
+++ lib/services/index.ts
@@ -3,3 +3,4 @@ export { default as accountServices } from "./accountServices";
 export { default as transactionServices } from "./transactionServices";
 export { default as categoryServivces } from "./categoryServivces";
 export { default as otpServices } from "./otpServices";
+export { default as initServices } from "./initServices";
---
+++ lib/services/initServices.ts
@@ -0,0 +1,39 @@
+import { conect } from "../db";
+import { throwError } from "../utils";
+import { InitSchemaType } from "../validations/initSchema";
+import accountServices from "./accountServices";
+import categoryServivces from "./categoryServivces";
+
+const initServices = {
+  async initializeUser(
+    info: InitSchemaType & { userID: string }
+  ): Promise<void> {
+    const { accountName, cardNumber, categoryName, currentBalance, userID } =
+      info;
+
+    // * Account & Category Services ===================== >
+    const { createCategory } = categoryServivces;
+    const { createAccount, isUniqueCardNumber } = accountServices;
+
+    // * Must be Uniq ================ >
+    const isUniqCard = await isUniqueCardNumber(cardNumber);
+    throwError(!isUniqCard, {
+      message: "شماره کارت نامعتر هست",
+      statusCode: 400,
+      type: "client",
+    }); // ! Might Throw Error ============= <
+    // * Run Query ================ >
+    await createCategory({ name: categoryName, user: userID }); // ! Might Throw Error ============= <
+    await createAccount(
+      {
+        accountName,
+        cardNumber,
+        currentBalance,
+        user: userID,
+      },
+      { uniqCheck: false }
+    ); // ! Might Throw Error ============= <
+  },
+};
+
+export default initServices;
