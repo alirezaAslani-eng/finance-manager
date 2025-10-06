b32560b2e9aa0e4f23dadfab50b866406d20c207
Alireza <alirezaaslane.mob7@gmail.com>
2025-10-06 21:09:18 +0330

    feat(API) an API to initialze user

---
+++ pages/api/init/index.ts
@@ -0,0 +1,42 @@
+import {
+  accountServices,
+  categoryServivces,
+  initServices,
+} from "@/lib/services";
+import { apiHandler, payloadToken, throwError } from "@/lib/utils";
+import { initSchema } from "@/lib/validations";
+import { handler_type } from "@/types/api.types";
+import { PayloadToken_type } from "@/types/user.types";
+
+const handler: handler_type = async (req, res) => {
+  const method = req.method as "POST";
+  throwError(method != "POST", {
+    message: "invalid method",
+    statusCode: 405,
+    type: "dev",
+  }); // ! MIght Throw Erro =========== <
+
+  // * Auth User ========================= >
+  const payloadInfo = payloadToken(req.cookies.token) as PayloadToken_type;
+  throwError(!payloadInfo, {
+    message: "لطفا اول ثبت نام کنید",
+    statusCode: 401,
+    type: "client",
+  }); // ! MIght Throw Erro =========== <
+  // * if no error read user's payload
+  const { _id: userID } = payloadInfo;
+
+  // * Body from Client ==================== >
+  const initInfo = initSchema.parse(req.body);
+
+  // * Account & Category Services ===================== >
+  const { initializeUser } = initServices;
+
+  // * add an category and an account ============= >
+  await initializeUser({ ...initInfo, userID });
+
+  // * Success Response ================= >
+  return res.status(204).json("");
+};
+
+export default apiHandler(handler);
