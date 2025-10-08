21ae7a35bcdc214d8ab1b4fab7125181b9059ddc
Alireza <alirezaaslane.mob7@gmail.com>
2025-10-08 11:25:03 +0330

    feat(module) transaction form module is able to add a transaction

--- components/module/Form/TransactionForm.tsx
+++ components/module/Form/TransactionForm.tsx
@@ -1,32 +1,57 @@
-import {
-  MuiButton,
-  MuiSelectInput,
-  MuiTextField,
-  SwitchButton,
-} from "@/components/ui";
+import { MuiSelectInput, MuiTextField, SwitchButton } from "@/components/ui";
+import { AuthContex } from "@/context";
 import { transactionSchema } from "@/lib/validations";
+import type { transactionSchemaType } from "@/lib/validations/transactionSchema";
 import { muiTheme } from "@/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Box, Grid, Typography, useTheme } from "@mui/material";
-import React from "react";
-import { useForm } from "react-hook-form";
+import React, { useContext, useMemo, useRef } from "react";
+import { Controller, useForm } from "react-hook-form";
 
 interface MyProps {
   edit?: boolean;
+  onSubmit: (info: transactionSchemaType) => Promise<void>;
 }
-function TransactionForm({ edit }: MyProps) {
+function TransactionForm({ edit, onSubmit }: MyProps) {
   // * Form Handler ======================== >
   const {
     control,
     register,
     formState: { errors, isSubmitting },
     handleSubmit,
+    setValue,
   } = useForm({
     resolver: zodResolver(transactionSchema),
+    defaultValues: {
+      account: "",
+      category: "",
+    },
   });
 
+  // * Auth Context to access to user's accounts and categories ==================== >
+  const {
+    userInfo: { accounts, categories },
+  } = useContext(AuthContex)!;
+
   // * Submiter ======================== >
-  const submiter = async () => {};
+  const submiter = async (form: unknown) => {
+    onSubmit && (await onSubmit(form as transactionSchemaType));
+  };
+
+  //  * Input select options ========================== >
+  const selectAccounts = useMemo(() => {
+    return accounts.map((account) => ({
+      text: `${account.cardNumber}  ${account.accountName}`,
+      value: account._id,
+    }));
+  }, [accounts]);
+
+  const categoriesSelect = useMemo(() => {
+    return categories.map((category) => ({
+      text: `${category.name} ویرابش`,
+      value: category._id,
+    }));
+  }, [categories]);
 
   // * style ================ >
   const { palette } = useTheme();
@@ -50,36 +75,57 @@ function TransactionForm({ edit }: MyProps) {
       </Typography>
 
       {/* Form ====================== > */}
-      <Box onSubmit={handleSubmit(submiter)} component={"form"}>
+      <Box component={"form"} onSubmit={handleSubmit(submiter)}>
         <Grid container spacing={2}>
           {/* Price Field =================================== > */}
           <Grid size={{ xs: 12, _600: 4 }}>
             <MuiTextField
               errorText={errors?.["amount"]?.message}
               textFieldProps={{
-                ...register("amount"),
+                disabled: isSubmitting,
+                ...register("amount", { valueAsNumber: true }),
                 placeholder: "مقدار تراکنش",
               }}
             />
           </Grid>
           {/* Account Field =================================== > */}
           <Grid size={{ xs: 12, _600: 4 }}>
-            <MuiSelectInput
-            //   errorText={errors?.["account"]?.message}
-            //   textFieldProps={{
-            //     ...register("account"),
-            //     placeholder: "برای کدوم حساب",
-            //   }}
+            <Controller
+              control={control}
+              name="account"
+              render={({ field }) => {
+                return (
+                  <MuiSelectInput
+                    errorText={errors?.["account"]?.message}
+                    selectItems={selectAccounts}
+                    inputProps={{
+                      ...field,
+                      disabled: isSubmitting,
+                      label: "انتخاب کارت",
+                    }}
+                  />
+                );
+              }}
             />
           </Grid>
           {/* Category Field =================================== > */}
           <Grid size={{ xs: 12, _600: 4 }}>
-            <MuiSelectInput
-            //   errorText={errors?.["category"]?.message}
-            //   textFieldProps={{
-            //     ...register("category"),
-            //     placeholder: "دسته بندی تراکنش",
-            //   }}
+            <Controller
+              control={control}
+              name="category"
+              render={({ field }) => {
+                return (
+                  <MuiSelectInput
+                    errorText={errors?.["category"]?.message}
+                    selectItems={categoriesSelect}
+                    inputProps={{
+                      ...field,
+                      disabled: isSubmitting,
+                      label: "دسته بندی",
+                    }}
+                  />
+                );
+              }}
             />
           </Grid>
         </Grid>
@@ -87,6 +133,7 @@ function TransactionForm({ edit }: MyProps) {
         <MuiTextField
           errorText={errors?.["reason"]?.message}
           textFieldProps={{
+            disabled: isSubmitting,
             ...register("reason"),
             placeholder: "دلیل تراکنش",
             multiline: true,
@@ -98,9 +145,12 @@ function TransactionForm({ edit }: MyProps) {
         <Box sx={{ mt: "20px" }}>
           <SwitchButton
             type="submit"
-            allButton={false}
+            onExpenseClick={() => setValue("type", "0")}
+            onIncomeClick={() => setValue("type", "1")}
+            disabled={isSubmitting}
             isExpenseActive
             isInComeActive
+            allButton={false}
           />
         </Box>
       </Box>
--- pages/my-panel/transactions/add.tsx
+++ pages/my-panel/transactions/add.tsx
@@ -1,13 +1,15 @@
 import { TransactionForm } from "@/components/module";
+import { useAddTransaction } from "@/hooks";
 import { PanelLayout } from "@/layout";
 import { PageComponent } from "@/types/page.types";
 import { Container } from "@mui/material";
 import React from "react";
 
 const add: PageComponent = () => {
+  const { addTransaction } = useAddTransaction();
   return (
     <Container>
-      <TransactionForm />
+      <TransactionForm onSubmit={addTransaction} />
     </Container>
   );
 };
