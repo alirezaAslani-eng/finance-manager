83ca3f7bb3dac8e0b9200520f494236ca6298b80
Alireza <alirezaaslane.mob7@gmail.com>
2025-10-09 14:05:38 +0330

    feat(page) ssr render page that's able to add and edit an account

--- pages/my-panel/account/add.tsx
+++ pages/my-panel/account/add.tsx
@@ -1,22 +1,40 @@
 import { AccountForm } from "@/components/module";
 import { PanelLayout } from "@/layout";
 import { PageComponent } from "@/types/page.types";
-import { Container } from "@mui/material";
-import { useRouter } from "next/router";
+import { Box, Container } from "@mui/material";
 import React from "react";
 import type { GlobalAppProps } from "@/pages/_app";
-import type { GetServerSideProps, GetServerSidePropsContext } from "next";
-import { useAddccount } from "@/hooks";
+import type { GetServerSidePropsContext } from "next";
+import { useAddccount, useEditAccount } from "@/hooks";
+import { AccountSchemaType } from "@/lib/validations/accountSchema";
+import { accountServices } from "@/lib/services";
+import { withAuth } from "@/lib/hoc";
+import { WrappedGetserverSideProps } from "@/types/ssr.types";
+import { parseDoc, redirect } from "@/lib/utils";
 
 interface PageProps {
   isEdit: boolean;
+  accountInfo?: AccountSchemaType;
+  accountId?: string;
 }
 
-const add: PageComponent<PageProps> = ({ isEdit }) => {
+const add: PageComponent<PageProps> = ({ isEdit, accountInfo, accountId }) => {
   const { addAccount } = useAddccount();
+  const { editAccount } = useEditAccount(accountId as string);
+
   return (
     <Container>
-      <AccountForm edit={isEdit} onSubmit={addAccount} />
+      {isEdit ? (
+        <AccountForm
+          onSubmit={editAccount}
+          edit={true}
+          defaultValues={accountInfo}
+        />
+      ) : (
+        <Box>
+          <AccountForm onSubmit={addAccount} />
+        </Box>
+      )}
     </Container>
   );
 };
@@ -24,9 +42,36 @@ const add: PageComponent<PageProps> = ({ isEdit }) => {
 add.Layout = PanelLayout;
 export default add;
 
-export const getServerSideProps: GetServerSideProps<
-  GlobalAppProps & PageProps
-> = async (context: GetServerSidePropsContext) => {
+
+
+
+
+const ssr: WrappedGetserverSideProps<GlobalAppProps | PageProps> = async (
+  context: GetServerSidePropsContext,
+  { user }
+) => {
   const { query } = context;
-  return { props: { isEdit: !!query.edit } };
+  const { getOneAccount } = accountServices;
+  // * if page render to edit account pass it's info ================= >
+  if (query.edit) {
+    const accountInfo = await getOneAccount(query.edit);
+
+    // * redirect user if account's id dos not exist
+    const isValidAccount = redirect(!accountInfo, { destination: "/404" }); // ! redirect <<<
+    if (isValidAccount) return isValidAccount;
+
+    // * Render ============ >
+    return {
+      props: {
+        isEdit: true,
+        accountInfo: parseDoc(accountInfo!),
+        ssrUserInfo: user,
+        accountId: query.edit,
+      },
+    };
+  }
+  // * if page render to add an account  ================= >
+  return { props: { isEdit: false } };
 };
+
+export const getServerSideProps = withAuth(ssr);
