73b706403ad9d59cefb7671a8b8a0f6201e3f266
Alireza <alirezaaslane.mob7@gmail.com>
2025-10-14 15:18:15 +0330

    feat(utils) check existing of _id and it's syntax

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -11,3 +11,4 @@ export { default as checkExist } from "./checkExist";
 export { default as sendVerifySMS } from "./sendVerifySMS";
 export { default as redirect } from "./redirect";
 export { default as parseDoc } from "./parseDoc";
+export { default as check_id } from "./check_id";
