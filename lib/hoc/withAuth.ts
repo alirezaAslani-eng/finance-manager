a516e2a53713eb3aa0ca92b36cd12e53483ab550
Alireza <alirezaaslane.mob7@gmail.com>
2025-10-14 15:16:36 +0330

    fix(hoc) withAuth checks if user hhas account but for some page it's not needed

--- lib/hoc/withAuth.ts
+++ lib/hoc/withAuth.ts
@@ -1,15 +1,26 @@
 import { GlobalAppProps } from "@/pages/_app";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import { accountServices, userServices } from "../services";
-import { parseDoc, redirect } from "../utils";
+import { checkOwnerOf, parseDoc, redirect } from "../utils";
 import { GetMeOutput } from "@/types/user.types";
 import { WrappedGetserverSideProps } from "@/types/ssr.types";
+import type { Model } from "mongoose";
 
-const withAuth = (ssr: WrappedGetserverSideProps): GetServerSideProps => {
+interface Options {
+  checkHasAccount?: boolean;
+}
+
+const withAuth = (
+  ssr: WrappedGetserverSideProps,
+  opt: Options = {}
+): GetServerSideProps => {
+  // * Options ================ >
+  const { checkHasAccount = true } = opt;
   const wrraped: GetServerSideProps<GlobalAppProps> = async (
     context: GetServerSidePropsContext
   ) => {
     const { req } = context;
+
     // * Services ================ >>
     const { getUserInfo } = userServices;
     const { hasAccount } = accountServices;
@@ -19,12 +30,14 @@ const withAuth = (ssr: WrappedGetserverSideProps): GetServerSideProps => {
     const isAuth = redirect(!!!userInfo, { destination: "/auth/signin" });
     if (isAuth) return isAuth;
 
-    // * Dose User have Account ============== >
-    const userHasAccount = await hasAccount(userInfo._id);
-    const has = redirect(!!!userHasAccount, {
-      destination: "/my-panel/init",
-    });
-    if (has) return has;
+    // * Dose User have Account (optional) ============== >
+    if (checkHasAccount) {
+      const userHasAccount = await hasAccount(userInfo._id);
+      const has = redirect(!!!userHasAccount, {
+        destination: "/my-panel/init",
+      }); // ! redirect to init page
+      if (has) return has;
+    }
 
     return ssr(context, { user: parseDoc(userInfo) });
   };
