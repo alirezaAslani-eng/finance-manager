f5673ad39c62fa87e3a40df9550a3e6ef0faa6d2
Alireza <alirezaaslane.mob7@gmail.com>
2025-10-15 13:11:56 +0330

    fix(hook) update recent transactions on client side after submit

--- hooks/useAddTransaction.ts
+++ hooks/useAddTransaction.ts
@@ -1,14 +1,20 @@
 import { postOneTransactions } from "@/api/post";
+import { keys } from "@/config/react-query";
 import { BadResponse } from "@/lib/utils";
 import { transactionSchemaType } from "@/lib/validations/transactionSchema";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 function useAddTransaction() {
+  const queryClient = useQueryClient();
   const { mutateAsync } = useMutation({ mutationFn: postOneTransactions });
 
   const addTransaction = async (transactionInfo: transactionSchemaType) => {
     try {
       await mutateAsync(transactionInfo);
+      // * update recent transactions after add one ========== >
+      await queryClient.invalidateQueries({
+        queryKey: keys.recntTransactions.all,
+      });
       // TODO Show Success Message ================== >
     } catch (err) {
       const error = err as BadResponse;
