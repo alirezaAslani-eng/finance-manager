269d12a9e785c5789213b5963c587820d8400159
Alireza <alirezaaslane.mob7@gmail.com>
2025-10-15 13:03:06 +0330

    feat(fetcher) delete a transaction

---
+++ api/delete/deleteOneTransaction.ts
@@ -0,0 +1,17 @@
+import type { BadResponse } from "@/lib/utils";
+
+const deleteOneTransaction = async (
+  id: string
+): Promise<true | BadResponse> => {
+  const res = await fetch(`api/transactions/${id}`, {
+    method: "DELETE",
+  });
+
+  if (!res.ok) {
+    const jsonRes = await res.json();
+    throw jsonRes as BadResponse;
+  }
+  return true;
+};
+
+export default deleteOneTransaction;
---
+++ api/delete/index.ts
@@ -0,0 +1,1 @@
+export { default as deleteOneTransaction } from "./deleteOneTransaction";
