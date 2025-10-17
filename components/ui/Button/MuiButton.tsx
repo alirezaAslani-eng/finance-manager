cfd7768a6aabacccc45162aee0459f02391c3730
Alireza <alirezaaslane.mob7@gmail.com>
2025-10-17 21:00:28 +0330

    fix(component) add default font for button

--- components/ui/Button/MuiButton.tsx
+++ components/ui/Button/MuiButton.tsx
@@ -5,9 +5,13 @@ import React, { PropsWithChildren } from "react";
 interface myProps {
   buttonProps?: ButtonProps;
 }
-function MuiButton({ children, buttonProps={} }: PropsWithChildren<myProps>) {
+function MuiButton({ children, buttonProps = {} }: PropsWithChildren<myProps>) {
   return (
-    <Button variant="contained" {...buttonProps}>
+    <Button
+      variant="contained"
+      {...buttonProps}
+      sx={{ fontSize: { xs: "1rem", md: "1.125rem" }, ...buttonProps?.sx }}
+    >
       {children}
     </Button>
   );
