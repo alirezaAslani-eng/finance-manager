a1a35c4fbaec078fa10463eb5993d9e177d811f9
Alireza <alirezaaslane.mob7@gmail.com>
2025-10-19 15:24:13 +0330

    feat(hook) edit transaction hook

--- hooks/index.ts
+++ hooks/index.ts
@@ -8,6 +8,7 @@ export { default as useBreakePoints } from "./useBreakePoints";
 export { default as useInitUser } from "./useInitUser";
 export { default as useAddTransaction } from "./useAddTransaction";
 export { default as useAddccount } from "./useAddccount";
+export { default as useEditTransaction } from "./useEditTransaction";
 export { default as useEditAccount } from "./useEditAccount";
 export { default as useRecentTransactions } from "./useRecentTransactions";
 export { default as useActiveAccount } from "./useActiveAccount";
---
+++ hooks/useEditTransaction.ts
@@ -0,0 +1,24 @@
+import { putEditTransaction } from "@/api/put";
+import { BadResponse } from "@/lib/utils";
+import { transactionEditSchemaType } from "@/lib/validations/transactionSchema";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+
+function useEditTransaction(_id: string) {
+  const { mutateAsync } = useMutation({ mutationFn: putEditTransaction });
+  const { replace } = useRouter();
+  const editTransaction = async (info: transactionEditSchemaType) => {
+    try {
+      await mutateAsync({ _id, ...info });
+      replace(`/my-panel/transactions/${_id}`);
+    } catch (err) {
+      const error = err as BadResponse;
+      console.log(error);
+      // todo show erro to user ======= >>
+    }
+  };
+
+  return { editTransaction };
+}
+
+export default useEditTransaction;
