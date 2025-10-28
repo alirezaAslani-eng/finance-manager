83f8deebea275e264ed85cef33e0af72375d454b
Alireza <alirezaaslane.mob7@gmail.com>
2025-10-28 20:58:05 +0330

    feat(component) side bar items has a button to open category modal

--- components/common/SideBar/SideBarItems.tsx
+++ components/common/SideBar/SideBarItems.tsx
@@ -1,23 +1,28 @@
-import { SideBarButton } from "@/components/ui";
-import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
-import ReceiptLongRoundedIcon from '@mui/icons-material/ReceiptLongRounded';
-import AddchartRoundedIcon from '@mui/icons-material/AddchartRounded';
-import AddCardRoundedIcon from '@mui/icons-material/AddCardRounded';
-import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
-import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
+import { ModalHandler, SideBarButton } from "@/components/ui";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import ReceiptLongRoundedIcon from "@mui/icons-material/ReceiptLongRounded";
+import AddchartRoundedIcon from "@mui/icons-material/AddchartRounded";
+import AddCardRoundedIcon from "@mui/icons-material/AddCardRounded";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import AddCircleOutlineRoundedIcon from "@mui/icons-material/AddCircleOutlineRounded";
 import { Box } from "@mui/material";
-import React from "react";
+import React, { useContext } from "react";
+import { ModalContext } from "@/context";
+import { CategoryModalForm } from "@/components/module";
 
 interface MyProp {
   onSideBarClick?: () => void;
 }
 function SideBarItems({ onSideBarClick = () => {} }: MyProp) {
+  // * Modal Context to open category modal ===== >
+  const { openAddCategoryModal } = useContext(ModalContext);
+
   return (
     <>
-      <Box onClick={onSideBarClick}>
+      <Box>
         <SideBarButton
+          buttonProps={{ onClick: onSideBarClick }}
           menuItem={{
-            id: crypto.randomUUID(),
             icon: <HomeRoundedIcon />,
             text: "داشبورد",
             link: "/my-panel",
@@ -25,8 +30,8 @@ function SideBarItems({ onSideBarClick = () => {} }: MyProp) {
         />
 
         <SideBarButton
+          buttonProps={{ onClick: onSideBarClick }}
           menuItem={{
-            id: crypto.randomUUID(),
             icon: <ReceiptLongRoundedIcon />,
             text: "تراکنش ها",
             link: "/my-panel/transactions",
@@ -34,8 +39,8 @@ function SideBarItems({ onSideBarClick = () => {} }: MyProp) {
         />
 
         <SideBarButton
+          buttonProps={{ onClick: onSideBarClick }}
           menuItem={{
-            id: crypto.randomUUID(),
             icon: <AddchartRoundedIcon />,
             text: "تراکنش جدید",
             link: "/my-panel/transactions/add",
@@ -43,35 +48,34 @@ function SideBarItems({ onSideBarClick = () => {} }: MyProp) {
         />
 
         <SideBarButton
+          buttonProps={{ onClick: onSideBarClick }}
           menuItem={{
-            id: crypto.randomUUID(),
             icon: <AddCardRoundedIcon />,
             text: "حساب جدید",
             link: "/my-panel/account/add",
           }}
         />
-
+        {/* // *  Open Category Modal ============= >  */}
         <SideBarButton
+          noLink
+          buttonProps={{ onClick: openAddCategoryModal }} // * Open Modal <<<<<
           menuItem={{
-            id: crypto.randomUUID(),
             icon: <AddCircleOutlineRoundedIcon />,
             text: "دسته بندی جدید",
+          }}
+        />
+
+        <SideBarButton
+          buttonProps={{ onClick: onSideBarClick }}
+          menuItem={{
+            icon: <AccountCircleRoundedIcon />,
+            text: "اطلاعات من",
             link: "",
           }}
         />
-
-        <SideBarButton
-          menuItem={{
-            id: crypto.randomUUID(),
-            icon: <AccountCircleRoundedIcon />,
-            text: "اطلاعات من",
-            link: "/my-panel/me",
-          }}
-        />
-      </Box>                                                                                                                                                                                                                                                                                                                        
+      </Box>
     </>
   );
 }
 
 export default SideBarItems;
-
