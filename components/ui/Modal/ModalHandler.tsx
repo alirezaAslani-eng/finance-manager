473b377806f2c6c7f4670012718637bdee1b04f3
Alireza <alirezaaslane.mob7@gmail.com>
2025-10-28 21:46:15 +0330

    fix(component) unecessary  import

--- components/ui/Modal/ModalHandler.tsx
+++ components/ui/Modal/ModalHandler.tsx
@@ -1,10 +1,5 @@
 import { PropsWithChildren } from "react";
-import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
 interface MyProps {
   isOpen: boolean;
   onClose?: () => void;
