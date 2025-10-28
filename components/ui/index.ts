a29950979854faea7f462f654f2d496a57d75ae9
Alireza <alirezaaslane.mob7@gmail.com>
2025-10-28 17:20:01 +0330

    feat(component) deletable chip (small box with a text and a delete icon)

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -21,3 +21,4 @@ export { default as NoData } from "./Fallback/NoData";
 export { default as MuiAlert } from "./Alert/MuiAlert";
 export { default as MuiToggleButton } from "./Button/MuiToggleButton";
 export { default as ModalHandler } from "./Modal/ModalHandler";
+export { default as DeletableChip } from "./Chip/DeletableChip";
