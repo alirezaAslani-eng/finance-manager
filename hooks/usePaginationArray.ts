dc3102c51f6f6e46dc055f80f3ef64362aff34d7
Alireza <alirezaaslane.mob7@gmail.com>
2025-10-28 17:30:03 +0330

    fix(code-style) add coment and a variable for page count

--- hooks/usePaginationArray.ts
+++ hooks/usePaginationArray.ts
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 interface Config {
   per?: number;
 }
-function usePaginationArray<T>(data: T[], options: Config | undefined = {}) {
+function usePaginationArray<T>(data: T[], options: Config = {}) {
   const { per = 4 } = options;
+
+  const pagedData: T[][] = []; // * Final Result <<<
   let start = 0;
   let end = per;
-  const pagedData: T[][] = [];
-  Array.from({ length: Math.ceil(data.length / 4) }, () => {
-    const slide = data.slice(start, end).map((item) => item);
+  const pageCount = data.length / per;
+  Array.from({ length: Math.ceil(pageCount) }, () => {
+    
+    const slide = data.slice(start, end).map((item) => item); // ! Might be Heavy <<
     pagedData.push(slide);
     start = end;
     end += 4;
