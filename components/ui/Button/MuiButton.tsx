d37e59898ddbc3ad516a6baad7905518490c1818
Alireza <alirezaaslane.mob7@gmail.com>
2025-10-30 22:16:28 +0330

    fix(component) reset button style by passing a prop

--- components/ui/Button/MuiButton.tsx
+++ components/ui/Button/MuiButton.tsx
@@ -1,16 +1,28 @@
+import { useTheme } from "@mui/material";
 import Button from "@mui/material/Button";
 import type { ButtonProps } from "@mui/material/Button";
 import React, { PropsWithChildren } from "react";
 
 interface myProps {
   buttonProps?: ButtonProps;
+  reset?: boolean;
 }
-function MuiButton({ children, buttonProps = {} }: PropsWithChildren<myProps>) {
+function MuiButton({
+  children,
+  buttonProps = {},
+  reset,
+}: PropsWithChildren<myProps>) {
+  const theme = useTheme();
   return (
     <Button
       variant="contained"
       {...buttonProps}
-      sx={{ fontSize: { xs: "1rem", md: "1.125rem" }, ...buttonProps?.sx }}
+      sx={{
+        ...Object.assign(reset ? { ...theme.custom.resetButton } : {}, {
+          fontSize: { xs: "1rem", md: "1.125rem" },
+          ...buttonProps?.sx,
+        }),
+      }}
     >
       {children}
     </Button>
