a16cf8d1e8bf634cbdd6d5ad1c887a6a168f5cca
Alireza <alirezaaslane.mob7@gmail.com>
2025-10-30 19:52:54 +0330

    fix(export)

--- lib/validations/index.ts
+++ lib/validations/index.ts
@@ -2,5 +2,5 @@ export { userSchema } from "./userSchema";
 export { sendCodeSchema, verifySchema } from "./otpSchema";
 export { accountSchema, activeAccountSchema } from "./accountSchema";
 export { transactionEditSchema, transactionSchema } from "./transactionSchema";
-export { default as categorySchema } from "./categorySchema";
+export { categoryEditSchema, categorySchema } from "./categorySchema";
 export { default as initSchema } from "./initSchema";
