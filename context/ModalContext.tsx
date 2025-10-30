5f1617502ec89510f09303df28c4f45c9ffbbe07
Alireza <alirezaaslane.mob7@gmail.com>
2025-10-30 18:44:48 +0330

    feat(context) add state for modal that edit a category

--- context/ModalContext.tsx
+++ context/ModalContext.tsx
@@ -15,21 +15,55 @@ function ModalProvider({ children }: PropsWithChildren) {
   // * The state of a modal that responsoble for adding category =================== >
   const [isOpenAddCategoryModal, setIsOpenAddCategoryModal] = useState(false);
 
+  // * Modla which is for editing category ========== >
+  const [editCategoryModalState, setEditCategoryModalState] = useState<
+    ModalProvidedValue["editCategoryModalState"]
+  >({
+    isOpen: false,
+    categoryId: "",
+    newName: "",
+  });
+
   // * Dispatcher --- >
   // * Category modal Dispatcher ================= >
-  const openAddCategoryModal = useCallback(() => {
-    setIsOpenAddCategoryModal(true);
-  }, []);
-  const closeAddCategoryModal = useCallback(() => {
-    setIsOpenAddCategoryModal(false);
-  }, []);
+  const openAddCategoryModal: ModalProvidedValue["openAddCategoryModal"] =
+    useCallback(() => {
+      setIsOpenAddCategoryModal(true);
+    }, []);
+
+  const closeAddCategoryModal: ModalProvidedValue["closeAddCategoryModal"] =
+    useCallback(() => {
+      setIsOpenAddCategoryModal(false);
+    }, []);
+
+  const openEditCategoryModal: ModalProvidedValue["openEditCategoryModal"] =
+    useCallback(
+      (
+        updatedCategory: Pick<
+          typeof editCategoryModalState,
+          "categoryId" | "newName"
+        >
+      ) => {
+        const { categoryId, newName } = updatedCategory;
+        setEditCategoryModalState({ isOpen: true, categoryId, newName });
+      },
+      []
+    );
+
+  const closeEditCategoryModal: ModalProvidedValue["closeEditCategoryModal"] =
+    useCallback(() => {
+      setEditCategoryModalState({ categoryId: "", isOpen: false, newName: "" });
+    }, []);
 
   // * Provider Values ================================================= >
   const providerValue: ModalProvidedValue = {
     // * Category modal Dispatcher ================= >
+    isOpenAddCategoryModal,
+    editCategoryModalState,
     closeAddCategoryModal,
     openAddCategoryModal,
-    isOpenAddCategoryModal,
+    closeEditCategoryModal,
+    openEditCategoryModal,
   };
   //   const providedValue: ModalProvidedValue = {};
   return <ModalContext value={providerValue}>{children}</ModalContext>;
--- context/types/ModalContext.types.ts
+++ context/types/ModalContext.types.ts
@@ -1,7 +1,17 @@
 interface ModalProvidedValue {
   isOpenAddCategoryModal: boolean;
+  editCategoryModalState: {
+    isOpen: boolean;
+    categoryId: string;
+    newName: string;
+  };
   openAddCategoryModal: () => void;
   closeAddCategoryModal: () => void;
+  openEditCategoryModal: (openState: {
+    newName: string;
+    categoryId: string;
+  }) => void;
+  closeEditCategoryModal: () => void;
 }
 
 export type { ModalProvidedValue };
