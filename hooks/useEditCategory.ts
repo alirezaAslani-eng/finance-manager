015719fba55af9c9cba0b42055b68b4c82f97ab9
Alireza <alirezaaslane.mob7@gmail.com>
2025-11-01 18:53:28 +0330

    feat(hook) find old name of category and return it, if _id was taken from hook

--- hooks/useEditCategory.ts
+++ hooks/useEditCategory.ts
@@ -5,23 +5,31 @@ import { CategoryEditSchemaType } from "@/lib/validations/categorySchema";
 import { useMutation } from "@tanstack/react-query";
 import { useContext } from "react";
 
+interface SubmitWithId {
+  editCategory: (info: CategoryEditSchemaType, __id: string) => Promise<void>;
+}
+interface InstancedWithID {
+  editCategory: (info: CategoryEditSchemaType) => Promise<void>;
+  oldCategoryName: string;
+}
+type ReturnType = SubmitWithId | InstancedWithID;
+function useEditCategory(_id: string): InstancedWithID;
+function useEditCategory(): SubmitWithId;
 
-// * when we need to pass the _id to the hook
-type SubmitNeedId = (
-  info: CategoryEditSchemaType,
-  __id: string
-) => Promise<void>;
-// * when we need to pass the _id each time when calling the submiter method
-type SubmitNoId = (info: CategoryEditSchemaType) => Promise<void>;
-
-function useEditCategory(_id: string): { editCategory: SubmitNoId };
-function useEditCategory(): { editCategory: SubmitNeedId };
-
-function useEditCategory(_id?: string) {
+function useEditCategory(_id?: string): ReturnType {
   const { mutateAsync } = useMutation({ mutationFn: putEditCategory });
 
   // * Auth Context to Edit Category in client =========== >
   const { editCategory: clientEdit } = useContext(AuthContex)!;
+
+  // * Auth Context to find old category ===== >
+  const {
+    userInfo: { categories },
+  } = useContext(AuthContex)!;
+  // * Find old category's name if _id is taken from hook parameters === >
+  const oldCategory = categories.find((cat) => {
+    return cat._id == _id;
+  });
 
   // * Submiter
   const editCategory = async (info: CategoryEditSchemaType, __id?: string) => {
@@ -43,7 +51,7 @@ function useEditCategory(_id?: string) {
     }
   };
 
-  return { editCategory };
+  return { editCategory, oldCategoryName: oldCategory?.name };
 }
 
 export default useEditCategory;
