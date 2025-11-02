eeed62f344ada1bab273f08484d5b76104745798
Alireza <alirezaaslane.mob7@gmail.com>
2025-11-02 21:31:08 +0330

    feat(page) after click on submit transaction show modal not submit

--- pages/my-panel/transactions/add.tsx
+++ pages/my-panel/transactions/add.tsx
@@ -1,16 +1,45 @@
 import { TransactionForm } from "@/components/module";
+import { TransactionDetailsModal, ModalHandler } from "@/components/ui";
 import { useAddTransaction } from "@/hooks";
 import { PanelLayout } from "@/layout";
+import { transactionSchemaType } from "@/lib/validations/transactionSchema";
 import { PageComponent } from "@/types/page.types";
 import { Container } from "@mui/material";
-import React from "react";
+import React, { useContext, useState } from "react";
 
 const add: PageComponent = () => {
-  const { addTransaction } = useAddTransaction();
+  // * Add Transaction Hook ==== >
+  const {
+    addTransaction,
+    closeModal,
+    openModal,
+    transaction,
+    categoryName,
+    isOpenModal,
+    isPending,
+  } = useAddTransaction();
+  // * Submit Tranaction ==== >
+  const SubmitTransaction = async () => {
+    addTransaction(transaction!);
+  };
+
   return (
-    <Container>
-      <TransactionForm onSubmit={addTransaction} />
-    </Container>
+    <>
+      <Container>
+        <TransactionForm onSubmit={openModal} />
+      </Container>
+      {/* // * Show Final Details And then Submit by Confriming modal */}
+      <ModalHandler isOpen={isOpenModal} onClose={closeModal}>
+        <TransactionDetailsModal
+          isSubmiting={isPending}
+          onReject={closeModal}
+          onAccept={SubmitTransaction}
+          amount={transaction?.amount}
+          category={categoryName}
+          type={transaction?.type == "0" ? "expense" : "income"}
+        />
+      </ModalHandler>
+    </>
   );
 };
 add.Layout = PanelLayout;
