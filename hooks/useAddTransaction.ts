14733c86969872105153154b307ddb8c16883856
Alireza <alirezaaslane.mob7@gmail.com>
2025-11-02 21:30:18 +0330

    feat(hook) add modal state in create transaction hook to have a beter controll on states

--- hooks/useAddTransaction.ts
+++ hooks/useAddTransaction.ts
@@ -1,12 +1,45 @@
 import { postOneTransactions } from "@/api/post";
 import { keys } from "@/config/react-query";
+import { AuthContex } from "@/context";
 import { BadResponse } from "@/lib/utils";
 import { transactionSchemaType } from "@/lib/validations/transactionSchema";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useContext, useState } from "react";
 
 function useAddTransaction() {
+  // * State for when wee need to cache the transaction info and submit it from state ===== >
+  const [transaction, setTransaction] = useState<transactionSchemaType | null>(
+    null
+  );
+
+  // * State of transaction modal to show final information ==== >
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  // * in the state below category prop is an _id  ==== >
+  const [categoryName, setCategoryName] = useState("");
+
+  // * Auth Context to access to user's categories ====== >
+  const { userInfo } = useContext(AuthContex)!;
+
+  // * Open Transaction Modal === >
+  const openModal = (transaction: transactionSchemaType) => {
+    setTransaction(transaction); // * set transaction before submit to show it on modal <<<
+    setCategoryName(
+      userInfo.categories.find((iteem) => iteem._id == transaction.category)
+        ?.name || ""
+    );
+    setIsOpenModal(true);
+  };
+
+  // * Close Transaction Modal === >
+  const closeModal = () => {
+    setIsOpenModal(false);
+  };
+
   const queryClient = useQueryClient();
-  const { mutateAsync } = useMutation({ mutationFn: postOneTransactions });
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: postOneTransactions,
+  });
 
   const addTransaction = async (transactionInfo: transactionSchemaType) => {
     try {
@@ -19,6 +52,9 @@ function useAddTransaction() {
       await queryClient.invalidateQueries({
         queryKey: keys.userInfo.all,
       });
+      // * Close Modal and clean state === >
+      closeModal();
+      setTransaction(null);
       // TODO Show Success Message ================== >
     } catch (err) {
       const error = err as BadResponse;
@@ -26,7 +62,15 @@ function useAddTransaction() {
     }
   };
 
-  return { addTransaction };
+  return {
+    addTransaction,
+    openModal,
+    closeModal,
+    transaction,
+    isOpenModal,
+    categoryName,
+    isPending,
+  };
 }
 
 export default useAddTransaction;
