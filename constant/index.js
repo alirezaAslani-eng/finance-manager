48d412fc49561bfb5ecd14f3404512e72076bcb0
Alireza <alirezaaslane.mob7@gmail.com>
2025-11-03 22:16:17 +0330

    feat(constant) add bank list to identyfy user's bank

--- constant/index.js
+++ constant/index.js
@@ -1,1 +1,2 @@
 export { default as breakepoints } from "./breakepoints";
+export { default as banks } from "./banks";
---
+++ constant/types/banks.types.ts
@@ -0,0 +1,8 @@
+interface Bank {
+  card_no: number;
+  bank_name: string;
+  bank_title: string;
+  bank_logo: string;
+}
+
+export type { Bank };
