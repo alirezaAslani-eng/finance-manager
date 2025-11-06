db87390f5efb8f6d755138358b3a18c43e398c03
Alireza <alirezaaslane.mob7@gmail.com>
2025-11-06 17:40:13 +0330

    feat(type) add transaction mongo and object type

--- types/transaction.types.ts
+++ types/transaction.types.ts
@@ -1,7 +1,8 @@
+import { Types } from "mongoose";
 import { Account_face } from "./account.types";
 import { Category_face } from "./category.types";
 import { User_face } from "./user.types";
-
+// * Basic Interface ===== >
 interface Transaction_face {
   type: "0" | "1"; // * 1 = income & 0 = Expense
   amount: number;
@@ -13,12 +14,37 @@ interface Transaction_face {
   isLatest: boolean;
 }
 
-type RecentTransactionType = Pick<
+// * Result of service or API Transaction type ======= >
+type Transaction = Pick<
   Transaction_face,
-  "amount" | "reason" | "type" | "isLatest"
-> & { category: Pick<Transaction_face["category"], "name"> } & {
-  _id: string;
+  "amount" | "isLatest" | "reason" | "type"
+> & {
   user: string;
-  createdAt: Date | string;
+  _id: string;
+  createdAt: string;
+  category: Pick<Transaction_face["category"], "name"> & { _id: string };
 };
-export type { Transaction_face, RecentTransactionType };
+type RecentTransactionType = Transaction;
+
+// * Array of transactions =============== >
+type TransactionList = Transaction[];
+
+// * Transaction Document ================= >
+interface MongoTransaction
+  extends Omit<Transaction, "user" | "_id" | "createdAt" | "category"> {
+  _id: Types.ObjectId;
+  user: Types.ObjectId;
+  createdAt: Date;
+  category: {
+    _id: Types.ObjectId;
+    name: string;
+  };
+}
+
+export type {
+  Transaction_face,
+  RecentTransactionType,
+  Transaction,
+  TransactionList,
+  MongoTransaction,
+};
