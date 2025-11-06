9a38b411967a021594af6342974750d9014f2e09
Alireza <alirezaaslane.mob7@gmail.com>
2025-11-06 18:08:40 +0330

    feat(page) initialize 50 transactions and render SSR page

--- pages/my-panel/transactions/index.tsx
+++ pages/my-panel/transactions/index.tsx
@@ -4,11 +4,20 @@ import {
   Transactions,
 } from "@/components/module";
 import { PanelLayout } from "@/layout";
+import { withAuth } from "@/lib/hoc";
+import { transactionServices } from "@/lib/services";
+import { GlobalAppProps } from "@/pages/_app";
 import { PageComponent } from "@/types/page.types";
+import { AllTransactionPageProps } from "@/types/pages/AllTransactionsProp.type";
+import { WrappedGetserverSideProps } from "@/types/ssr.types";
 import { Box } from "@mui/material";
 import React, { useState } from "react";
 
-const index: PageComponent = () => {
+const index: PageComponent<AllTransactionPageProps> = ({
+  hasMore,
+  lastId,
+  transactions,
+}) => {
   const [isOpenSidebar, setIsOpenSidebar] = useState<boolean>(false);
   //  * Events ====================>
   const openSidebar = () => {
@@ -25,7 +34,7 @@ const index: PageComponent = () => {
       {/* Sidebar Advanced Filter ========================= > */}
       <SidebarFilter open={isOpenSidebar} onClose={closeSidebar} />
       {/* Transactions ===================== > */}
-      <Box sx={{mt:"20px"}}>
+      <Box sx={{ mt: "20px" }}>
         <Transactions />
       </Box>
     </Box>
@@ -34,3 +43,25 @@ const index: PageComponent = () => {
 
 index.Layout = PanelLayout;
 export default index;
+
+const ssr: WrappedGetserverSideProps<
+  GlobalAppProps & AllTransactionPageProps
+> = async (context, { user }) => {
+  const { initialTransactions } = transactionServices;
+  // * Get 50 initialzed transactions ==== >
+  const initializeTransaction = await initialTransactions(user._id);
+  const { initial_transactions, lastId } = initializeTransaction;
+  return {
+    props: {
+      ssrUserInfo: user,
+      transactions: initial_transactions,
+      lastId,
+      // * less than 50 means user can't request to server to load more transactions
+      hasMore: initial_transactions.length < 50 ? false : true,
+    },
+  };
+};
+
+const getServerSideProps = withAuth(ssr);
+
+export { getServerSideProps };
