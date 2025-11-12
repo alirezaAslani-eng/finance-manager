bc4def3e20df9e725fd0d9541a24aa5cdb8690c2
Alireza <alirezaaslane.mob7@gmail.com>
2025-11-12 22:30:13 +0330

    feat(type) querie type for filtering transactions

--- types/api/transactionApi.types.ts
+++ types/api/transactionApi.types.ts
@@ -7,5 +7,35 @@ interface AllTransactionResponse {
 }
 interface FilteredTransactionResonse extends AllTransactionResponse {}
 
+interface TrnasactionFilterURLQueries {
+  /**
+   * Only Transactions before This Date
+   */
+  fromDate: Date;
+  /**
+   * Only Transactions after This Date
+   */
+  toDate: Date;
+  /**
+   * Only Transactions with amount greater than minAmount or equal
+   */
+  minAmount: number;
+  /**
+   * Only Transactions with amount less than maxAmount or equal
+   */
+  maxAmount: number;
+  /**
+   * 0 = Expense & 1 = Income
+   */
+  Type: 0 | 1;
+  /**
+   * true = Show Oldest Transactions & false = Default (Latest)
+   */
+  old: boolean;
+}
 
-export type { AllTransactionResponse, FilteredTransactionResonse };
+export type {
+  AllTransactionResponse,
+  FilteredTransactionResonse,
+  TrnasactionFilterURLQueries,
+};
