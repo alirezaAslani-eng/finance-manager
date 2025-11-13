477daf11b848748714a50e1f71b1f82fc92fb5fe
Alireza <alirezaaslane.mob7@gmail.com>
2025-11-13 15:12:32 +0330

    fix(type) this type refers to url bar queries so everything is string or string[]

--- types/api/transactionApi.types.ts
+++ types/api/transactionApi.types.ts
@@ -7,31 +7,18 @@ interface AllTransactionResponse {
 }
 interface FilteredTransactionResonse extends AllTransactionResponse {}
 
-interface TrnasactionFilterURLQueries {
-  /**
-   * Only Transactions before This Date
-   */
-  fromDate: Date;
-  /**
-   * Only Transactions after This Date
-   */
-  toDate: Date;
-  /**
-   * Only Transactions with amount greater than minAmount or equal
-   */
-  minAmount: number;
-  /**
-   * Only Transactions with amount less than maxAmount or equal
-   */
-  maxAmount: number;
-  /**
-   * 0 = Expense & 1 = Income
-   */
-  Type: 0 | 1;
-  /**
-   * true = Show Oldest Transactions & false = Default (Latest)
-   */
-  old: boolean;
+type URLFilterQueries =
+  | "fromDate"
+  | "toDate"
+  | "minAmount"
+  | "maxAmount"
+  | "type"
+  | "old"
+  | "accounts"
+  | "categories";
+interface TrnasactionFilterURLQueries
+  extends Partial<Record<URLFilterQueries, string | string[]>> {
+  filter: string;
 }
 
 export type {
