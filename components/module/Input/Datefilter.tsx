5b295eb30f72dcead4efdeb6ec2efbcb5f47bd3e
Alireza <alirezaaslane.mob7@gmail.com>
2025-11-19 17:58:17 +0330

    fix(module) add cancel buttons and their events

--- components/module/Input/Datefilter.tsx
+++ components/module/Input/Datefilter.tsx
@@ -1,19 +1,31 @@
-import { Box, BoxProps } from "@mui/material";
-import { DateField } from "@/components/ui";
-
+import { Box, BoxProps, SxProps } from "@mui/material";
+import { DateField, MuiButton } from "@/components/ui";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 interface myProps {
   containerProps?: BoxProps;
   fromOnchange?: (date: Date) => void;
   toOnchange?: (date: Date) => void;
+  onCancelToDate?: () => void;
+  onCancelFromDate?: () => void;
   fromDateVal?: Date | null;
   toDateVal?: Date | null;
 }
+const cancelButton_sx: SxProps = {
+  position: "absolute",
+  zIndex: "2",
+  left: "10px",
+  top: "50%",
+  transform: "translateY(-50%)",
+  borderRadius: "999px",
+};
 function Datefilter({
   containerProps,
   fromOnchange,
   toOnchange,
   fromDateVal,
   toDateVal,
+  onCancelFromDate,
+  onCancelToDate,
 }: myProps) {
   const to_Date = (date: Date) => {
     toOnchange && toOnchange(date);
@@ -25,19 +37,55 @@ function Datefilter({
     <Box {...containerProps}>
       <Box sx={{ display: "flex", flexDirection: "column", gap: "15px" }}>
         {/* Start Date ==================== > */}
-        <DateField
-          placeholder="از تاریخ : "
-          onChange={from_Date}
-          // * undefined = default date value
-          value={fromDateVal ?? undefined}
-        />
+        <Box position={"relative"}>
+          {fromDateVal && (
+            <MuiButton
+              reset
+              buttonProps={{
+                variant: "text",
+                color: "error",
+                onClick: (e) => {
+                  e.stopPropagation();
+                  onCancelFromDate && onCancelFromDate();
+                },
+                sx: cancelButton_sx,
+              }}
+            >
+              <CloseRoundedIcon />
+            </MuiButton>
+          )}
+          <DateField
+            placeholder="از تاریخ : "
+            onChange={from_Date}
+            // * undefined = default date value
+            value={fromDateVal ?? undefined}
+          />
+        </Box>
         {/* End Date ==================== > */}
-        <DateField
-          placeholder="تا تاریخ : "
-          onChange={to_Date}
-          // * undefined = default date value
-          value={toDateVal ?? undefined}
-        />
+        <Box position={"relative"}>
+          {toDateVal && (
+            <MuiButton
+              reset
+              buttonProps={{
+                variant: "text",
+                color: "error",
+                onClick: (e) => {
+                  e.stopPropagation();
+                  onCancelToDate && onCancelToDate();
+                },
+                sx: cancelButton_sx,
+              }}
+            >
+              <CloseRoundedIcon />
+            </MuiButton>
+          )}
+          <DateField
+            placeholder="تا تاریخ : "
+            onChange={to_Date}
+            // * undefined = default date value
+            value={toDateVal ?? undefined}
+          />
+        </Box>
       </Box>
     </Box>
   );
