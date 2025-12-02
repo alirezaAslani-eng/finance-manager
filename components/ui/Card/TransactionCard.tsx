f9b70fa31816022dcdaebe5ca1827e6bfdcea84e
Alireza <alirezaaslane.mob7@gmail.com>
2025-12-02 20:09:44 +0330

    fix(component) change all the styles and layout structure. removed not needed parts and props

--- components/ui/Card/TransactionCard.tsx
+++ components/ui/Card/TransactionCard.tsx
@@ -1,221 +1,170 @@
-import Card from "@mui/material/Card";
 import Box from "@mui/material/Box";
-import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
-import DoneRoundedIcon from "@mui/icons-material/DoneRounded";
 import Typography from "@mui/material/Typography";
-import ArrowOutwardRoundedIcon from "@mui/icons-material/ArrowOutwardRounded";
-import { MuiButton, MuiProgress, TextPrice } from "@/components/ui";
-import { useTheme } from "@mui/material/styles";
-import Link from "next/link";
+import { TextPrice } from "@/components/ui";
 import { muiTheme } from "@/utils";
 import { RecentTransactionType } from "@/types/transaction.types";
-import DeleteOutlineRoundedIcon from "@mui/icons-material/DeleteOutlineRounded";
-import { useState } from "react";
 import { useDate } from "@/hooks";
-type Transaction = Pick<
-  RecentTransactionType,
-  "_id" | "amount" | "category" | "reason" | "createdAt" | "type" | "isLatest"
+import { Skeleton, Stack, SxProps } from "@mui/material";
+import CallReceivedRoundedIcon from "@mui/icons-material/CallReceivedRounded";
+import Link from "next/link";
+
+type Transaction = Partial<
+  Pick<
+    RecentTransactionType,
+    "amount" | "category" | "createdAt" | "type" | "_id"
+  >
 >;
 interface MyProps extends Transaction {
-  onRemove?: (id: string) => void;
-  isRemoving?: boolean;
+  isPending?: boolean;
 }
 
+const shared_icon_sx: SxProps = {
+  borderRadius: "14px",
+  aspectRatio: "1/1",
+  height: { xs: "35px", _540: "40px" },
+};
+
+const shared_category_sx: SxProps = { fontSize: { xs: "16px", _540: "24px" } };
+const shared_date_sx: SxProps = { xs: "14px", _540: "16px" };
 const TransactionCard = ({
-  _id,
   amount = 0,
   category,
   createdAt,
-  reason,
   type,
-  isLatest,
-  isRemoving,
-  onRemove = () => {},
+  _id = "",
+  isPending,
 }: MyProps) => {
-  // * is in Remove state ======== >
-  const [isRemoveState, setIsRemoveState] = useState<boolean>(false);
-
-  // * MUI theme ======= >
-  const theme = useTheme();
-  const {
-    palette: { grey, mode, success, error },
-    alpha,
-  } = theme;
+  // * income = 1 / expense = 0 ===== >
+  const isIncome = type === "1";
 
   // * Date ========== >
-  const { date } = useDate(createdAt);
+  const { date, time } = useDate(new Date(createdAt ?? ""));
 
-  // * Remove event === >
-  const removeMe = () => {
-    onRemove(_id);
-  };
-
-  // * State handler === >
-  const removeStateToggle = () => {
-    setIsRemoveState((prev) => !prev);
-  };
   return (
-    <Card
-      variant="outlined"
-      sx={{
-        position: "relative",
-        borderRadius: "20px",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-      }}
-    >
-      {/* // * cover for remove state ==== > */}
-      {isRemoveState && (
-        <Box
-          sx={{
-            width: "100%",
-            height: "100%",
-            position: "absolute",
-            backgroundColor: alpha(error.main, 0.2),
-          }}
-        ></Box>
-      )}
-      <Box sx={{ p: 2 }}>
-        <Box
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            alignItems: "center",
-            // * Responsive Flex ===================== >
-            justifyContent: { xs: "center", md: "space-between" },
-            flexDirection: { xs: "column", md: "row" },
-          }}
-        >
-          {/* // * Title ===================== > */}
-          <Typography
-            component="p"
-            // * Responsive fontSize ===================== >
-            sx={{
-              fontSize: {
-                xs: "18px",
-                sm: "22px",
-                color: muiTheme(mode, { light: grey[800], dark: grey[100] }),
-              },
-            }}
-          >
-            {category?.name}
-          </Typography>
-          {/* // * Price ================ > */}
-          <TextPrice
-            price={amount}
-            type={type == "0" ? "expense" : "income"}
-            // * Responsive fontSize ===================== >
-            priceProps={{ sx: { fontSize: { xs: "24px", sm: "26px" } } }}
-          />
-        </Box>
-        {/* // * Description ================ > */}
-        <Link
-          href={{
-            pathname: "/my-panel/transactions/[id]",
-            query: { id: _id },
-          }}
-        >
-          <Typography
-            variant="subtitle1"
-            sx={{
-              // * Responsive textAlign ===================== >
-              textAlign: { xs: "center", sm: "right" },
-              color: muiTheme(mode, {
-                light: alpha(grey[900], 0.6),
-                dark: alpha(grey[50], 0.6),
-              }),
-            }}
-          >
-            {reason}
-          </Typography>
-        </Link>
-      </Box>
-
-      {/* // * Footer Section ================= > */}
+    <Link href={`transactions/${_id}`}>
       <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          p: "10px",
-          borderTop: "1px solid",
-          borderColor:
-            type == "0" ? alpha(error.main, 0.2) : alpha(success.main, 0.2),
-          boxShadow: `0px 30px 50px 0px ${
-            type == "0" ? alpha(error.main, 0.5) : alpha(success.main, 0.5)
-          }`,
+        // * Box is wrapper
+        sx={(theme) => {
+          const {
+            palette: { grey, mode },
+          } = theme;
+          return {
+            p: "14px",
+            borderRadius: "12px",
+            transition: "scale 150ms ease",
+            cursor: "pointer",
+            bgcolor: muiTheme(mode, {
+              dark: grey[900],
+              light: grey[100],
+            }),
+            ":hover": {
+              scale: "1.01",
+            },
+          };
         }}
       >
-        <Box sx={{ display: "flex", alignItems: "center", gap: "10px" }}>
-          {/* // * Dont show info button when state is remove ========== > */}
-          {!isRemoveState && (
-            <Link
-              href={{
-                pathname: "/my-panel/transactions/[id]",
-                query: { id: _id },
+        {/* // * Used Stack to centeralize Type Icon with Content */}
+        <Stack flexDirection={"row"} alignItems={"center"} gap={"14px"}>
+          {/* // * Type Icon === > */}
+          {!isPending ? (
+            <Box
+              sx={(theme) => {
+                const {
+                  palette: { error, success },
+                  alpha,
+                } = theme;
+                return {
+                  ...shared_icon_sx,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  color: isIncome ? success.main : error.main,
+                  bgcolor: isIncome
+                    ? alpha(success.main, 0.2)
+                    : alpha(error.main, 0.2),
+                };
               }}
             >
-              <MuiButton
-                buttonProps={{
-                  sx: {
-                    ...theme.custom.resetButton,
-                    borderRadius: "999px",
-                    display: "flex",
-                    alignItems: "center",
-                    gap: "5px",
-                    p: "10px",
-                  },
-                }}
-              >
-                <ArrowOutwardRoundedIcon />
-              </MuiButton>
-            </Link>
-          )}
-          {/* // * Remove Section ==================== > */}
-
-          {isLatest && (
-            <MuiButton
-              buttonProps={{
-                disabled: isRemoving,
-                onClick: isRemoveState ? removeMe : removeStateToggle,
-                variant: isRemoveState ? "contained" : "outlined",
-                color: "error",
-                sx: {
-                  ...theme.custom.resetButton,
-                  borderRadius: "999px",
-                  p: "10px",
-                },
-              }}
-            >
-              {isRemoveState ? (
-                <DoneRoundedIcon />
+              {isIncome ? (
+                <CallReceivedRoundedIcon />
               ) : (
-                <DeleteOutlineRoundedIcon />
+                <CallReceivedRoundedIcon sx={{ rotate: "180deg" }} />
               )}
-            </MuiButton>
+            </Box>
+          ) : (
+            <Skeleton variant="rounded" sx={shared_icon_sx} />
           )}
-          {/* // * Close Remove State =========== > */}
-          {isRemoveState && (
-            <MuiButton
-              buttonProps={{
-                disabled: isRemoving,
-                onClick: removeStateToggle,
-                sx: {
-                  ...theme.custom.resetButton,
-                  borderRadius: "999px",
-                  p: "10px",
-                },
-              }}
+
+          {/* // * Transactions Content (Price) | Category =============================== > */}
+          <Box flex={"1"} minWidth={"0"}>
+            <Stack
+              justifyContent={"space-between"}
+              flexDirection={"row"}
+              alignItems={"center"}
             >
-              <CloseRoundedIcon />
-            </MuiButton>
-          )}
-        </Box>
-        {/* Date ===================== > */}
-        <Typography>{date}</Typography>
+              {/* // * Category ==== > */}
+              {!isPending ? (
+                <Typography
+                  fontFamily={"var(--peyda-md)"}
+                  sx={shared_category_sx}
+                >
+                  {category?.name ?? ""}
+                </Typography>
+              ) : (
+                <Skeleton
+                  variant="text"
+                  sx={shared_category_sx}
+                  width={"100px"}
+                />
+              )}
+              {/* // * Amount ==== > */}
+              {!isPending ? (
+                <Box
+                  alignSelf={{ xs: "start", _540: "center" }}
+                  sx={(theme) => {
+                    const {
+                      palette: { error, success },
+                      alpha,
+                    } = theme;
+                    return {
+                      borderRadius: "8px",
+                      p: "4px 8px",
+                      bgcolor: isIncome
+                        ? alpha(success.main, 0.1)
+                        : alpha(error.main, 0.1),
+                    };
+                  }}
+                >
+                  <TextPrice
+                    fontSize={{
+                      xs: "14px",
+                      _540: "18px",
+                    }}
+                    price={amount}
+                    type={isIncome ? "income" : "expense"}
+                  />
+                </Box>
+              ) : (
+                <Skeleton
+                  variant="text"
+                  sx={shared_category_sx}
+                  width={"100px"}
+                />
+              )}
+            </Stack>
+            {/* // * Date ======== > */}
+            {!isPending ? (
+              <Typography
+                mt={"5px"}
+                sx={shared_date_sx}
+              >{`${date} | ${time}`}</Typography>
+            ) : (
+              <Skeleton variant="text" sx={shared_date_sx} width={"150px"} />
+            )}
+          </Box>
+        </Stack>
       </Box>
-    </Card>
+    </Link>
   );
 };
 
