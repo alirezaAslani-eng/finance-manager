332fdf837aa6d00a141e3228ebf89dba66118480
Alireza <alirezaaslane.mob7@gmail.com>
2025-12-02 23:21:33 +0330

    feat(module) (changed static to dynamic) : Transaction module can dynamically renders transactionCard by using it's hook and handling loading

--- components/module/Transactions/Transactions.tsx
+++ components/module/Transactions/Transactions.tsx
@@ -1,9 +1,18 @@
-import { MuiButton, TransactionCard } from "@/components/ui";
-import { Box, Grid, useTheme } from "@mui/material";
-
+import { MuiButton, NoTransactions, TransactionCard } from "@/components/ui";
+import { useGetInfinitTransactions } from "@/hooks";
+import { Box } from "@mui/material";
+import LoadeingErrorHandler from "../WaitHandler/LoadeingErrorHandler";
+import TransactionSkeletonList from "../SkeletonGenerator/TransactionSkeletonList";
+import { transactionCursorConfig } from "@/lib/constant";
 const Transactions = () => {
-  // * Style =========== >
-  const theme = useTheme();
+  const {
+    transactions,
+    loadMore,
+    emprtArrayReason,
+    isFiltering,
+    isFetchingNextPage,
+    hasNextPage,
+  } = useGetInfinitTransactions();
   return (
     <Box sx={{ width: "100%" }}>
       <Box
@@ -18,35 +27,41 @@ const Transactions = () => {
           gap: "20px",
         }}
       >
-        <TransactionCard />
-        <TransactionCard />
-        <TransactionCard />
-        <TransactionCard />
-        <TransactionCard />
-        <TransactionCard />
-        <TransactionCard />
-        <TransactionCard />
-        <TransactionCard />
-        <TransactionCard />
-        <TransactionCard />
-        <TransactionCard />
-        <TransactionCard />
-        <TransactionCard />
-        <TransactionCard />
+        <LoadeingErrorHandler
+          // * Render Skeleton ================= >
+          isLoading={{
+            check: isFiltering,
+            loading: <TransactionSkeletonList count={4} />,
+          }}
+        >
+          {transactions.map((trs) => {
+            return <TransactionCard key={trs._id} {...trs} />;
+          })}
+        </LoadeingErrorHandler>
+        {/* // * Skeleton For Load more state =============== >  */}
+        {isFetchingNextPage && (
+          <TransactionSkeletonList
+            count={transactionCursorConfig.loadMoreLimit}
+          />
+        )}
       </Box>
-      {/* Lazy Fetch Button ==================== > */}
-      <Box sx={{ display: "flex", justifyContent: "center", mt: "20px" }}>
+      {/* // * Lazy Fetch Button ==================== > */}
+      <Box display={"flex"} justifyContent={"center"} mt={"20px"}>
+        {!isFetchingNextPage && hasNextPage && (
         <MuiButton
+            reset
           buttonProps={{
+              variant: "outlined",
+              onClick: loadMore,
             sx: {
-              ...theme.custom.resetButton,
-              borderRadius: "999px",
-              p: "18px",
+                borderRadius: "14px",
+                p: "8px 12px",
             },
           }}
         >
-          {"بیشتر"}
+            {"نمایش بیشتر"}
         </MuiButton>
+        )}
       </Box>
     </Box>
   );
