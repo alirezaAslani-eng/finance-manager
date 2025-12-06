03d35f909dae03dc06e06e1c9ddea4d58866066a
Alireza <alirezaaslane.mob7@gmail.com>
2025-12-06 17:19:05 +0330

    feat(utility) modularized nex.js font loaders

---
+++ utils/font/index.ts
@@ -0,0 +1,2 @@
+export { default as dana_md } from "./dana_md";
+export { default as peyda_md } from "./peyda_md";
---
+++ utils/font/peyda_md.ts
@@ -0,0 +1,7 @@
+import font from "next/font/local";
+const peyda_md = font({
+  src: "../../assets/fonts/PeydaWeb-Medium.woff",
+  variable: "--peyda-md",
+});
+
+export default peyda_md;
