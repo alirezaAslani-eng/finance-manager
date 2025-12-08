ae46ed4ce52884dbe7479435858cda79ada92b13
Alireza <alirezaaslane.mob7@gmail.com>
2025-12-08 22:04:44 +0330

    fix(comonent) remove uneccessary components

--- components/ui/CheckBox/MuiCheckBox.tsx
+++
@@ -1,8 +0,0 @@
-import Checkbox from "@mui/material/Checkbox";
-import React from "react";
-
-function MuiCheckBox() {
-  return <Checkbox defaultChecked />;
-}
-
-export default MuiCheckBox;
--- components/ui/Chip/MuiChip.tsx
+++
@@ -1,9 +0,0 @@
-import { Chip } from "@mui/material";
-import React from "react";
-interface MyProps {
-  text?: string;
-}
-function MuiChip({  text }: MyProps) {
-  return <Chip label={text} />;
-}
-export default MuiChip;
