3a38538e17797491c24413972389a56965e61d41
Alireza <alirezaaslane.mob7@gmail.com>
2025-12-11 22:48:38 +0330

    fix(query-key) separate the serializer because there might be added other keys in th future that need this function

--- lib/integration/react-query/keys/keyAllTransactions.ts
+++ lib/integration/react-query/keys/keyAllTransactions.ts
@@ -1,22 +1,26 @@
 import { FilterSchemaType } from "@/lib/validations/transactionSchema";
-import { KeyAllTransactions } from "./types";
+import { KeyAllTransactions, TransactionsQueryKey } from "./types";
 
 const keyAllTransactions: KeyAllTransactions = {
   mainKey: "transactions",
   all(filters) {
-    return [
-      keyAllTransactions.mainKey,
-      // * Filter Parmeters ==== >
-      JSON.stringify(filters["type"]),
-      JSON.stringify(filters["old"]),
-      JSON.stringify(filters["minAmount"]),
-      JSON.stringify(filters["maxAmount"]),
-      JSON.stringify(filters["fromDate"]),
-      JSON.stringify(filters["toDate"]),
-      JSON.stringify(filters["categories"]),
-      JSON.stringify(filters["accounts"]),
-    ] as const;
+    return serializeDynamicKeys(filters);
   },
 };
 
 export default keyAllTransactions;
+
+function serializeDynamicKeys(filters: FilterSchemaType): TransactionsQueryKey {
+  return [
+    "transactions",
+    // * Filter Parmeters ==== >
+    JSON.stringify(filters["type"]),
+    JSON.stringify(filters["old"]),
+    JSON.stringify(filters["minAmount"]),
+    JSON.stringify(filters["maxAmount"]),
+    JSON.stringify(filters["fromDate"]),
+    JSON.stringify(filters["toDate"]),
+    JSON.stringify(filters["categories"]),
+    JSON.stringify(filters["accounts"]),
+  ] as const;
+}
