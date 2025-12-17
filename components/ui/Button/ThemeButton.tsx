58064c0868fb749c2110b93ca130f14ae6326526
Alireza <alirezaaslane.mob7@gmail.com>
2025-12-17 14:52:51 +0330

    fix(usage of ControllThemeContext) used its hook instead

--- components/ui/Button/ThemeButton.tsx
+++ components/ui/Button/ThemeButton.tsx
@@ -1,6 +1,6 @@
-import React, { useContext } from "react";
+import React from "react";
 import { useTheme } from "@mui/material/styles";
-import { ControllThemeContext } from "@/context/ControllThemeContext";
+import { useControllTheme } from "@/context";
 import BedtimeRoundedIcon from "@mui/icons-material/BedtimeRounded";
 import WbSunnyRoundedIcon from "@mui/icons-material/WbSunnyRounded";
 import { Button } from "@mui/material";
@@ -10,7 +10,7 @@ function ThemeButton() {
     palette: { mode },
   } = useTheme();
   // * use context to change theme mode ======= >
-  const { changeMode } = useContext(ControllThemeContext);
+  const { changeMode } = useControllTheme();
 
   return (
     <Button
