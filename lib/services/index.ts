fd803e7dac56a9bdd84057f3c6e15936ff1037bb
Alireza <alirezaaslane.mob7@gmail.com>
2025-12-20 18:03:48 +0330

    fix(removed old service and database folder architecture that aren't imported anymore)

--- lib/services/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as userServices } from "./userServices";
-export { default as accountServices } from "./accountServices";
-export { default as transactionServices } from "./transactionServices";
-export { default as categoryServivces } from "./categoryServivces";
-export { default as otpServices } from "./otpServices";
-export { default as initServices } from "./initServices";
