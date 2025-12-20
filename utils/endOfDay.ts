9a725bc59961c1705181fbec41290d982d8c9549
Alireza <alirezaaslane.mob7@gmail.com>
2025-12-20 17:59:08 +0330

    refactor(func's name and comments was wrong)

--- utils/endOfDay.ts
+++ utils/endOfDay.ts
@@ -2,11 +2,11 @@
  * end of day -> setHours(23, 59, 59, 999)
  * notice : it wont change your date's refrence directy
  */
-const startOfDay = (date: Date): Date => {
+const endOfDay = (date: Date): Date => {
   const _date = new Date(date);
-  // * start of the day ================ >
+  // * end of the day ================ >
   _date.setHours(23, 59, 59, 999);
   return _date;
 };
 
-export default startOfDay;
+export default endOfDay;
