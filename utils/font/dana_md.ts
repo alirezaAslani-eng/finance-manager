ffef0bf3598c54ac5265f5bd0d5385de5902d245
Alireza <alirezaaslane.mob7@gmail.com>
2026-01-05 16:01:25 +0330

    fix(removed unused font)

--- utils/font/dana_md.ts
+++ utils/font/dana_md.ts
@@ -1,6 +1,6 @@
 import font from "next/font/local";
 const dana_md = font({
-  src: "../../assets/fonts/DanaFaNum-DemiBold.woff2",
+  src: "../../assets/fonts/DanaFaNum-Medium.woff2",
   variable: "--dana-md",
 });
 export default dana_md;
