2fb3b69a0114faf1463418eedf8b25fedc9109ca
Alireza <alirezaaslane.mob7@gmail.com>
2026-01-07 14:39:47 +0330

    refactor(move a utility from "/utils" to "/packages/mui/theme/helpers")
    because this is not a general utility it's in the domain of material-ui

--- packages/mui/index.ts
+++ packages/mui/index.ts
@@ -3,4 +3,5 @@
  */
 export { default as theme } from "./theme/theme";
 export { getThemeWithMode } from "./theme/helpers";
+export { muiTheme } from "./theme/helpers";
 export type { ThemeMode } from "./theme/types";
--- packages/mui/theme/helpers/index.ts
+++ packages/mui/theme/helpers/index.ts
@@ -1,1 +1,2 @@
 export { default as getThemeWithMode } from "./getThemeWithMode";
+export { default as muiTheme } from "./muiTheme";
