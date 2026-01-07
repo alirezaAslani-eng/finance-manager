ac7634b0df9cd2d31f1904d9133de44696df9310
Alireza <alirezaaslane.mob7@gmail.com>
2026-01-07 13:13:18 +0330

    fix(remove unused options)

--- utils/identifyDate.ts
+++ utils/identifyDate.ts
@@ -1,31 +1,10 @@
-import { endOfDay } from ".";
-import startOfDay from "./startOfDay";
 
-interface Options {
-  /**
-   * End of The day of your Date
-   */
-  endOfDaye?: boolean;
-  /**
-   * Beginnig of The day of your Date
-   */
-  startOfDay?: boolean;
-}
-const identifyDate = (
-  val: unknown,
-  opt: Options = { endOfDaye: false, startOfDay: false }
-): Date | null => {
-  // * Options =============== >
-  const { endOfDaye: isEnd, startOfDay: isStart } = opt;
-
+const identifyDate = (val: unknown): Date | null => {
   if (typeof val === "string" && val.length) {
     // * Check Date ==== >
     const date = new Date(val);
     let identifiedDate: Date | null = isNaN(date.getTime()) ? null : date;
     if (!identifiedDate) return null;
-    // * change houre (optional)
-    identifiedDate = isStart ? startOfDay(date) : date;
-    identifiedDate = isEnd ? endOfDay(date) : date;
     return identifiedDate;
   }
   return null;
