08103643f5a30e8627a5ae95ea5404a4ffc9138f
Alireza <alirezaaslane.mob7@gmail.com>
2026-01-12 20:49:49 +0330

    refactor(modularized types)
    other modules might need these types

--- packages/mui/theme/types/theme.d.ts
+++ packages/mui/theme/types/theme.d.ts
@@ -1,40 +1,19 @@
 import type { SxProps, Theme } from "@mui/material/styles";
+import { CustomStyle, FontSizes, Shades } from "./theme.types";
 
-interface Shades {
-  50?: string;
-  100?: string;
-  200?: string;
-  300?: string;
-  400?: string;
-  500?: string;
-  600?: string;
-  700?: string;
-  800?: string;
-  900?: string;
-}
-interface FontSizes {
-  base?: string;
-  xs?: string;
-  sm?: string;
-  lg?: string;
-  xl?: string;
-  "2xl"?: string;
-  "3xl"?: string;
-  "4xl"?: string;
-}
 
-/**
- * Customized reusable styles
- */
-interface CustomStyle {
-  circleButton: SxProps<Theme>;
-  noScroll: SxProps<Theme>;
-}
 
 declare module "@mui/material/styles" {
   interface PaletteColor extends Shades {}
   interface SimplePaletteColorOptions extends Shades {}
-
+  interface Palette {
+    black: string;
+    white: string;
+  }
+  interface PaletteOptions {
+    black?: string;
+    white?: string;
+  }
   interface Theme {
     custom?: CustomStyle;
   }
@@ -42,12 +21,8 @@ declare module "@mui/material/styles" {
     custom?: CustomStyle;
   }
 
-  interface TypographyVariantsOptions {
-    fontSizes?: FontSizes;
-  }
-  interface TypographyVariants {
-    fontSizes?: FontSizes;
-  }
+  interface TypographyVariantsOptions extends FontSizes {}
+  interface TypographyVariants extends FontSizes {}
 
   interface BreakpointOverrides {
     _700: true;
--- packages/mui/theme/types/theme.types.ts
+++ packages/mui/theme/types/theme.types.ts
@@ -1,4 +1,36 @@
+import { Theme } from "@emotion/react";
+import type { SxProps, TypographyStyle } from "@mui/material";
 
 type ThemeMode = "dark" | "light";
 
-export type { ThemeMode };
+interface Shades {
+  50?: string;
+  100?: string;
+  200?: string;
+  300?: string;
+  400?: string;
+  500?: string;
+  600?: string;
+  700?: string;
+  800?: string;
+  900?: string;
+}
+interface FontSizes {
+  base?: TypographyStyle;
+  xs?: TypographyStyle;
+  sm?: TypographyStyle;
+  lg?: TypographyStyle;
+  xl?: TypographyStyle;
+  "2xl"?: TypographyStyle;
+  "3xl"?: TypographyStyle;
+  "4xl"?: TypographyStyle;
+}
+
+/**
+ * Customized reusable styles
+ */
+interface CustomStyle {
+  circleButton: SxProps<Theme>;
+  noScroll: SxProps<Theme>;
+}
+export type { ThemeMode, CustomStyle, FontSizes, Shades };
