f31a29f1c18ba50b0e27f69bd34eae61fd957af0
Alireza <alirezaaslane.mob7@gmail.com>
2026-01-18 11:40:57 +0330

    refactor(re-styled signup page)
    implemented new Form component and useCheckSignupInfo hook

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,20 +1,91 @@
-import { RegisterForm } from "@/components/module";
-import { Box, Container } from "@mui/material";
-import React from "react";
+import { Form } from "@/components/ui";
+import { SignupContext } from "@/context";
+import { useCheckSignupInfo } from "@/hooks";
+import { signupSchema } from "@/lib/validations";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Box } from "@mui/material";
+import React, { useContext } from "react";
+import { useForm } from "react-hook-form";
 
 function signup() {
+  const { signupInfo } = useContext(SignupContext);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: zodResolver(signupSchema().omit({ otpCode: true })),
+    defaultValues: signupInfo! ?? undefined,
+  });
+  const { checkInfo, isSuccessCheck } = useCheckSignupInfo();
   return (
     <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100svh",
-      }}
+      display={"flex"}
+      justifyContent={"center"}
+      px={"20px"}
+      pt={{ xs: "30px", sm: "60px" }}
     >
-      <Container sx={{ display: "flex", justifyContent: "center" }}>
-        <RegisterForm />
-      </Container>
+      <Form
+        muiProps={{ onSubmit: handleSubmit(checkInfo) }}
+        isSubmiting={isSubmitting || isSuccessCheck}
+      >
+        <Form.Title>{"هزینه یار"}</Form.Title>
+        <Form.FormBox>
+          <Form.FormBox.FormTitle>
+            {"شروع یک مدیریت حرفه ای"}
+          </Form.FormBox.FormTitle>
+          <Form.FormBox.SubTitle>
+            {"حساب دارم"}
+            <Form.FormBox.SubTitle.Link href="/auth/signin">
+              {"ورود"}
+            </Form.FormBox.SubTitle.Link>
+          </Form.FormBox.SubTitle>
+
+          <Form.FormBox.Input
+            error={!!errors?.fullName}
+            placeholder="نام و نام خانوادگی"
+            {...register("fullName")}
+          >
+            {errors?.fullName?.message}
+          </Form.FormBox.Input>
+
+          <Form.FormBox.Input
+            error={!!errors?.userName}
+            placeholder="نام کاربری"
+            {...register("userName")}
+          >
+            {errors?.userName?.message}
+          </Form.FormBox.Input>
+
+          <Form.FormBox.Input
+            error={!!errors?.email}
+            placeholder="ایمیل"
+            {...register("email")}
+          >
+            {errors?.email?.message}
+          </Form.FormBox.Input>
+
+          <Form.FormBox.Input
+            error={!!errors?.phone}
+            placeholder="شماره موبایل"
+            {...register("phone")}
+          >
+            {errors?.phone?.message}
+          </Form.FormBox.Input>
+
+          <Form.FormBox.Input
+            error={!!errors?.password}
+            placeholder="رمز عبور"
+            type="password"
+            {...register("password")}
+          >
+            {errors?.password?.message}
+          </Form.FormBox.Input>
+
+          <Form.FormBox.SubmitButton>{"ثبت نام"}</Form.FormBox.SubmitButton>
+        </Form.FormBox>
+        <Form.RuleDescription />
+      </Form>
     </Box>
   );
 }
