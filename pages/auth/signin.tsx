8f4eb9957fedcdcec7037156a2f787754daa968d
Alireza <alirezaaslane.mob7@gmail.com>
2026-01-18 11:31:04 +0330

    refactor(rename page's name)
    changed name  from "check-phone" to "signin"  because it makes sence that this page is the start point of logining process

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,20 +1,56 @@
-import { LoginForm } from "@/components/module";
-import { Box, Container } from "@mui/material";
-import React from "react";
+import { Form } from "@/components/ui";
+import { useAuth } from "@/context";
+import { useCheckUserPhone } from "@/hooks";
+import { signupSchema } from "@/lib/validations";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Box } from "@mui/material";
+import { useForm } from "react-hook-form";
 
 function signin() {
+  const { userInfo } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: zodResolver(signupSchema().pick({ phone: true })),
+    defaultValues: { phone: userInfo.phone },
+  });
+  const { checkPhone, isSuccess } = useCheckUserPhone();
   return (
     <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100svh",
-      }}
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      height={"100svh"}
+      px={"24px"}
     >
-      <Container sx={{display:"flex",justifyContent:"center"}}>
-        <LoginForm />
-      </Container>
+      <Form
+        isError={!!errors?.phone}
+        isSubmiting={isSubmitting || isSuccess}
+        muiProps={{ onSubmit: handleSubmit(checkPhone) }}
+      >
+        <Form.Title>{"هزینه یار"}</Form.Title>
+        <Form.FormBox>
+          <Form.FormBox.FormTitle>
+            {"ورود با شماره موبایل"}
+          </Form.FormBox.FormTitle>
+
+          <Form.FormBox.SubTitle>
+            {"ثبت نام نکردی؟"}
+            <Form.FormBox.SubTitle.Link href="/auth/signup">
+              {"ثبت نام"}
+            </Form.FormBox.SubTitle.Link>
+          </Form.FormBox.SubTitle>
+
+          <Form.FormBox.Input
+            placeholder="شماره موبایل"
+            {...register("phone")}
+          />
+          <Form.FormBox.SubmitButton>{"ورود"}</Form.FormBox.SubmitButton>
+        </Form.FormBox>
+        <Form.RuleDescription />
+      </Form>
     </Box>
   );
 }
