fcc6ead25a8744a85372a6f468bbf80611475069
Alireza <alirezaaslane.mob7@gmail.com>
2026-02-10 20:28:57 +0330

    feat(added types)
    ## The advantages of these types are that we can use them for compound components
    
    - **MemoizedComponentType** : Type of Components that are memoized.
    - **ComponentType** : Type of typical components

--- types/component.types.ts
+++ types/component.types.ts
@@ -1,7 +1,38 @@
+import { FC, MemoExoticComponent } from "react";
+
 interface ComponentFetchState<TData = any> {
   isError?: boolean;
   isPending?: boolean;
   data?: TData;
 }
 
-export type { ComponentFetchState };
+type CompoundComponentsType = {
+  [ComponentName: string]:
+    | ComponentType<any, any>
+    | MemoizedComponentType<any, any>;
+};
+
+/**
+ * Must be implemented by 'as' :
+ * @example
+ * const Component = memo(YourComponent) as MemoizedComponentType<PropsType,CompoundComponents>
+ */
+type MemoizedComponentType<
+  TProps = {},
+  CompoundComponents extends CompoundComponentsType = {},
+> = MemoExoticComponent<FC<TProps>> & {
+  [ComponentPropsType in keyof CompoundComponents]: CompoundComponents[ComponentPropsType];
+};
+
+/**
+ * ### Usage :
+ * @example
+ * const Component:ComponentType<PropsType,CompoundComponents> = YourComponent
+ */
+type ComponentType<
+  TProps = {},
+  CompoundComponents extends CompoundComponentsType = {},
+> = FC<TProps> & {
+  [ComponentPropsType in keyof CompoundComponents]: CompoundComponents[ComponentPropsType];
+};
+export type { ComponentFetchState, MemoizedComponentType, ComponentType };
