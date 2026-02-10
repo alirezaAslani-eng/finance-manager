e67abac0404d693194ca357deca964aa788c1712
Alireza <alirezaaslane.mob7@gmail.com>
2026-02-10 20:41:25 +0330

    fix(hook useAddEventListener)
    the prop `el` must be required

--- hooks/app/useAddEventListener.ts
+++ hooks/app/useAddEventListener.ts
@@ -1,25 +1,32 @@
+import { isValidElement } from "@/utils";
 import React, { DependencyList, RefObject, useEffect, useRef } from "react";
 
 interface Options<
   TEventType extends keyof WindowEventMap,
-  TElement extends HTMLElement | Window | null
+  TElement extends HTMLElement | Window | null,
 > {
-  el?: TElement;
+  el: TElement;
   type: TEventType;
   listener: (this: TElement, e: WindowEventMap[TEventType]) => void;
 }
 
 function useAddEventListener<
   TEventType extends keyof WindowEventMap = any,
-  TElement extends HTMLElement | Window | null = Window
->({ el, type, listener }: Options<TEventType, TElement>, deps: DependencyList) {
+  TElement extends HTMLElement | Window | null = Window,
+>(
+  { el, type, listener }: Options<TEventType, TElement>,
+  deps: DependencyList = [],
+) {
   useEffect(() => {
+    const Element = isValidElement(el);
+    if (!Element) return;
     // @ts-ignore
-    el ?? window.addEventListener(type, listener);
+    Element.addEventListener(type, listener);
     return () => {
-      if (!el) return;
+      const Element = isValidElement(el);
+      if (!Element) return;
       // @ts-ignore
-      el ?? window.removeEventListener(type, listener);
+      Element.removeEventListener(type, listener);
     };
   }, deps);
 }
