2f05973714f81d9f16b4b69c1f6148e900c26757
Alireza <alirezaaslane.mob7@gmail.com>
2026-02-11 01:21:48 +0330

    refactor(structure of WheelSelector Component changed)
    Changed from Compound Component structure into a typical structure because the previous usage was hard to use and not clean

--- components/ui/WheelSelector/WheelSelector.tsx
+++ components/ui/WheelSelector/WheelSelector.tsx
@@ -1,203 +1,158 @@
 import { useWheelSelector } from "@/hooks";
-import { contextCreator } from "@/utils";
-import type {
-  Compound_ButtonProps,
-  Compound_OptionProps,
-  Compound_Selector,
-  Compound_WheelProps,
-  Compound_WheelShadowProps,
-  WheelSelectorProps,
-} from "@/components/ui/types";
-import { Box, Button } from "@mui/material";
-import { UseWheelSelectorReturnType } from "@/hooks/app/types";
-import { identifySxProp, muiTheme } from "@/packages/mui";
-const { Context, useCreatedContext } =
-  contextCreator<UseWheelSelectorReturnType>();
+import type { WheelSelectorComponent } from "@/components/ui/types";
+import type { OptionTagBasicProps } from "@/types/component.types";
+import {
+  Box,
+  BoxProps,
+  Button,
+  ButtonProps,
+  SxProps,
+  Theme,
+} from "@mui/material";
+import { muiTheme } from "@/packages/mui";
+import { useCallback } from "react";
+import { OptimizedMap } from "@/components/module";
+import KeyboardArrowUpRoundedIcon from "@mui/icons-material/KeyboardArrowUpRounded";
+import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
+const sx_parentBox: SxProps = {
+  ["& .active-option"]: {
+    fontWeight: 700,
+  },
+};
+const sx_shadowBox: SxProps<Theme> = (tm: Theme) => {
+  const { palette, alpha } = tm;
+  return {
+    "::after": {
+      content: `""`,
+      position: "absolute",
+      width: "100%",
+      height: "25%",
+      zIndex: "3",
+      backgroundImage: `linear-gradient(180deg, ${palette.background.default}, ${alpha(palette.background.default, 0.5)}, ${alpha(palette.background.default, 0)})`,
+      top: 0,
+      left: 0,
+      pointerEvents: "none",
+    },
+    "::before": {
+      content: `""`,
+      position: "absolute",
+      width: "100%",
+      height: "25%",
+      zIndex: "3",
+      backgroundImage: `linear-gradient(0deg, ${palette.background.default}, ${alpha(palette.background.default, 0.5)}, ${alpha(palette.background.default, 0)})`,
+      bottom: 0,
+      left: 0,
+      pointerEvents: "none",
+    },
+    width: "100%",
+  };
+};
+const sx_scrollBox: SxProps<Theme> = (tm: Theme) => {
+  return {
+    height: "150px",
+    width: "100%",
+    scrollBehavior: "smooth",
+    scrollSnapType: "y mandatory",
+    overflowY: "auto",
+    overflowX: "hidden",
+    ...(tm.custom!.noScroll as object),
+  };
+};
+const sx_selectorBox: SxProps<Theme> = (tm) => {
+  const { palette, alpha } = tm;
+  return {
+    height: "50px",
+    width: "100%",
+    borderTop: `1px solid ${muiTheme(palette.mode, { dark: alpha("#fff", 0.3), light: alpha("#000", 0.3) })}`,
+    borderBottom: `1px solid ${muiTheme(palette.mode, { dark: alpha("#fff", 0.3), light: alpha("#000", 0.3) })}`,
+    position: "absolute",
+    zIndex: 2,
+    left: 0,
+    top: "50%",
+    transform: "translateY(-50%)",
+    pointerEvents: "none",
+  };
+};
+const sx_option: SxProps = {
+  height: "50px",
+  width: "100%",
+  scrollSnapAlign: "center",
+  scrollSnapStop: "always",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  bgcolor: "transparent",
+  fontFamily: "var(--dana-md)",
+};
+const shared_button_props: ButtonProps = {
+  variant: "outlined",
+  size: "small",
+  sx: (tm) => tm.custom!.circleButton as object,
+};
 
-function WheelSelector({
-  children,
+const WheelSelector: WheelSelectorComponent = function ({
   onChangeOption,
-  value,
-  boxProps,
-}: WheelSelectorProps) {
-  return (
-    // todo style the component based on MUI
-    <Context
-      value={useWheelSelector({
-        onChange: onChangeOption,
-        value,
-        active_className: "active-option",
-      })}
-    >
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        gap="8px"
-        flexDirection="column"
-        {...boxProps}
-        sx={{
-          ["& .active-option"]: {
-            fontWeight: 700,
-          },
-        }}
-      >
-        {children}
-      </Box>
-    </Context>
-  );
-}
+  selctedOption,
+  options = [],
+}) {
+  const wheel = useWheelSelector({
+    active_className: "active-option",
+    value: selctedOption,
+    onChange: onChangeOption,
+  });
 
-// * Compound Components ---------------------------------- >
-WheelSelector.WheelShadow = (props: Compound_WheelShadowProps) => {
+  const optionsProps = useCallback(
+    (item: OptionTagBasicProps, index: number): BoxProps<"option"> => {
+      return {
+        component: "option",
+        sx: sx_option,
+        value: item?.value,
+        children: item?.text,
+        ...wheel.registerEachOption(index),
+      };
+    },
+    [wheel.registerEachOption, sx_option],
+  );
   return (
     <Box
-      {...props}
-      sx={(tm) => {
-        const { palette, alpha } = tm;
-        return {
-          "::after": {
-            content: `""`,
-            position: "absolute",
-            width: "100%",
-            height: "25%",
-            zIndex: "3",
-            backgroundImage: `linear-gradient(180deg, ${palette.background.default}, ${alpha(palette.background.default, 0.5)}, ${alpha(palette.background.default, 0)})`,
-            top: 0,
-            left: 0,
-            pointerEvents: "none",
-          },
-          "::before": {
-            content: `""`,
-            position: "absolute",
-            width: "100%",
-            height: "25%",
-            zIndex: "3",
-            backgroundImage: `linear-gradient(0deg, ${palette.background.default}, ${alpha(palette.background.default, 0.5)}, ${alpha(palette.background.default, 0)})`,
-            bottom: 0,
-            left: 0,
-            pointerEvents: "none",
-          },
-          width: "100%",
-          ...identifySxProp(tm, props?.sx),
-        };
-      }}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      gap="8px"
+      flexDirection="column"
       position={"relative"}
+      width={"100%"}
+      sx={sx_parentBox}
     >
-      {props?.children}
+      {/* // * ---------------- Prev Button ---------------- */}
+      <Button {...shared_button_props} onClick={wheel.prevOption}>
+        <KeyboardArrowUpRoundedIcon />
+      </Button>
+
+      {/* // * ---------------- Scroll Shadow ---------------- */}
+      <Box sx={sx_shadowBox} position={"relative"}>
+        {/* // * ---------------- Scroll Container ---------------- */}
+        <Box sx={sx_scrollBox} {...wheel.registerScrollContainer()}>
+          <Box height={"50px"} width={"100%"}></Box>
+          {/* // * ---------------- Options ---------------- */}
+          <OptimizedMap
+            Component={Box}
+            data={options}
+            injector={optionsProps}
+          />
+          <Box height={"50px"} width={"100%"}></Box>
+        </Box>
+      </Box>
+
+      {/* // * ---------------- Next Button ---------------- */}
+      <Button {...shared_button_props} onClick={wheel.nextOption}>
+        <KeyboardArrowDownRoundedIcon />
+      </Button>
+
+      {/* // * ---------------- Selector ---------------- */}
+      <Box sx={sx_selectorBox} />
     </Box>
-  );
-};
-
-WheelSelector.Wheel = (props: Compound_WheelProps) => {
-  const { registerScrollContainer } = useCreatedContext();
-  return (
-    <Box
-      {...props}
-      sx={(tm) => {
-        return {
-          height: "150px",
-          width: "100%",
-          scrollBehavior: "smooth",
-          scrollSnapType: "y mandatory",
-          overflowY: "auto",
-          overflowX: "hidden",
-          ...identifySxProp(tm, props?.sx),
-          ...(tm.custom!.noScroll as object),
-        };
-      }}
-      {...registerScrollContainer()}
-    >
-      <Box height={"50px"} width={"100%"}></Box>
-      {props?.children}
-      <Box height={"50px"} width={"100%"}></Box>
-    </Box>
-  );
-};
-
-WheelSelector.Option = (props: Compound_OptionProps) => {
-  const { registerEachOption } = useCreatedContext();
-  return (
-    <Box
-      sx={{
-        height: "50px",
-        width: "100%",
-        scrollSnapAlign: "center",
-        scrollSnapStop: "always",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        bgcolor: "transparent",
-        fontFamily: "var(--dana-md)",
-      }}
-      {...props}
-      {...registerEachOption(props.tabIndex)}
-      component={"option"}
-    >
-      {props?.children}
-    </Box>
-  );
-};
-
-WheelSelector.Button = ({
-  buttonProps,
-  children,
-  action_type,
-}: Compound_ButtonProps) => {
-  const { prevOption, nextOption } = useCreatedContext();
-  return (
-    <Button
-      {...buttonProps}
-      onClick={(e) => {
-        if (action_type === "next") nextOption();
-        else if (action_type === "prev") prevOption();
-        buttonProps?.onClick && buttonProps.onClick(e);
-      }}
-    >
-      {children}
-    </Button>
-  );
-};
-
-WheelSelector.Selector = (props: Compound_Selector) => {
-  return (
-    <Box
-      sx={(tm) => {
-        const { palette, alpha } = tm;
-        return {
-          height: "50px",
-          width: "100%",
-          borderTop: `1px solid ${muiTheme(palette.mode, { dark: alpha("#fff", 0.3), light: alpha("#000", 0.3) })}`,
-          borderBottom: `1px solid ${muiTheme(palette.mode, { dark: alpha("#fff", 0.3), light: alpha("#000", 0.3) })}`,
-          position: "absolute",
-          zIndex: 2,
-          left: 0,
-          top: "50%",
-          transform: "translateY(-50%)",
-          pointerEvents: "none",
-          ...identifySxProp(tm, props?.sx),
-        };
-      }}
-      {...props}
-    ></Box>
   );
 };
 
 export default WheelSelector;
-
-/**
- * ? Usage :
-<WheelSelector>
- <WheelSelector.Button action_type="prev">قبلی</WheelSelector.Button>
- <WheelSelector.WheelShadow>
-   <WheelSelector.Wheel>
- <WheelSelector.Option tabIndex={0}>1</WheelSelector.Option>
- <WheelSelector.Option tabIndex={1}>2</WheelSelector.Option>
- <WheelSelector.Option tabIndex={2}>3</WheelSelector.Option>
- <WheelSelector.Option tabIndex={3}>4</WheelSelector.Option>
-   </WheelSelector.Wheel>
-   <WheelSelector.Selector />
- </WheelSelector.WheelShadow>
- <WheelSelector.Button action_type="next">بعدی</WheelSelector.Button>
-</WheelSelector>
- */
