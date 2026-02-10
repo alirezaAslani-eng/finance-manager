438226d4589762f09e7c867e5e59d0743fb50f7c
Alireza <alirezaaslane.mob7@gmail.com>
2026-02-10 20:37:10 +0330

    fix(utility buildArray)
    sometimes, we needed to modify each index while building an array

--- lib/utils/app/buildArray.ts
+++ lib/utils/app/buildArray.ts
@@ -1,4 +1,12 @@
-const buildArray = (length: number) =>
-  Array.from({ length }, (_, index) => index);
+function buildArray<TItem = number>(
+  length: number,
+  indexModifier?: (index: number) => TItem,
+): TItem[] {
+  return Array.from({ length }, (_, index): TItem => {
+    if (indexModifier) return indexModifier(index);
+    // @ts-ignore
+    return index; // * it only happens when indexModifier is undefined
+  });
+}
 
 export default buildArray;
