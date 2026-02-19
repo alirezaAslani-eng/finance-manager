4dc6bb885be1bb4bdda8a3c32f9675f7930b5fb7
Alireza <alirezaaslane.mob7@gmail.com>
2026-02-19 21:30:25 +0330

    fix(TextField Component Label)
    The label of Textfield must be on right side to sync with our RTL design

--- packages/mui/theme/overrides/textField.ts
+++ packages/mui/theme/overrides/textField.ts
@@ -6,11 +6,31 @@ const textField = {
     lang: "fa",
     dir: "rtl",
   },
+
   styleOverrides: {
     root: {
       ["& .MuiOutlinedInput-root"]: {
         borderRadius: "16px",
         height: "100%",
+      },
+
+      "& label": {
+        right: 0,
+        top: 0,
+        width: "fit-content",
+        transform: "translate(-14px , 16px)",
+      },
+      "& label.MuiInputLabel-sizeSmall": {
+        transform: "translate(-14px , 9px)",
+      },
+      "& .MuiInputLabel-shrink": {
+        transform: "translate(-14px , -9px) !important",
+      },
+      "& fieldset legend": {
+        direction: "rtl",
+        textAlign: "right",
+        fontFamily: "inherit",
+        fontSize: "unset",
       },
     },
   },
