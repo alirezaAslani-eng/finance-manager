6552d4b02fb9100e5c8c6c169f585d99ae3dcb54
Alireza <alirezaaslane.mob7@gmail.com>
2026-03-05 11:11:30 +0330

    fix(TransactionCard Component)
    This component must not navigate users to another page, because when user click on it, it triggers a modal to show the full information of a transaction

--- components/module/Transactions/RecentTransactions.tsx
+++ components/module/Transactions/RecentTransactions.tsx
@@ -1,42 +1,45 @@
 import { TransactionCard } from "@/components/ui";
+import { useModalState } from "@/hooks";
+import DialogBottomSheet from "@/packages/mui/styled-components/Dialog/DialogBottomSheet";
+import { Transaction, TransactionList } from "@/types/transaction.types";
 import { Box } from "@mui/material";
 import type { BoxProps } from "@mui/material";
-
+import TransactionInfo from "./TransactionInfo";
+const fake_transactions: TransactionList = [
+  {
+    _id: "123",
+    account: "",
+    amount: 2400000,
+    category: { _id: "", name: "دسته بندی" },
+    createdAt: new Date().toISOString(),
+    isLatest: true,
+    reason: "",
+    type: "0",
+    user: "",
+  },
+];
 const RecentTransactions = (boxProps: BoxProps) => {
+  const { isOpenModal, modalInfo, closeModal, openModal } =
+    useModalState<Transaction>({
+      isParentModal: true,
+    });
+
   return (
-    <Box
-      display={"flex"}
-      gap={"14px"}
-      flexDirection={"column"}
-      justifyContent={"center"}
-      alignItems={"center"}
-      minHeight={{ xs: "326px", sm: "414px" }}
-      {...boxProps}
-    >
-      <TransactionCard
-        amount={40000000}
-        category={{ _id: "", name: "خرج خونه" }}
-        createdAt={new Date().toISOString()}
-        type="0"
-      />
-      <TransactionCard
-        amount={40000000}
-        category={{ _id: "", name: "خرج خونه" }}
-        createdAt={new Date().toISOString()}
-        type="0"
-      />
-      <TransactionCard
-        amount={40000000}
-        category={{ _id: "", name: "خرج خونه" }}
-        createdAt={new Date().toISOString()}
-        type="0"
-      />
-      <TransactionCard
-        amount={40000000}
-        category={{ _id: "", name: "خرج خونه" }}
-        createdAt={new Date().toISOString()}
-        type="0"
-      />
+    <Box {...boxProps}>
+      {fake_transactions.map((transaction) => {
+        return (
+          <Box
+            width={"100%"}
+            alignSelf={"start"}
+            onClick={() => openModal(transaction)}
+          >
+            <TransactionCard {...transaction} />
+          </Box>
+        );
+      })}
+      <DialogBottomSheet open={!!modalInfo && isOpenModal} onClose={closeModal}>
+        {!!modalInfo && <TransactionInfo {...modalInfo} />}
+      </DialogBottomSheet>
     </Box>
   );
 };
--- components/ui/Card/TransactionCard.tsx
+++ components/ui/Card/TransactionCard.tsx
@@ -6,7 +6,6 @@ import { RecentTransactionType } from "@/types/transaction.types";
 import { useDate } from "@/hooks";
 import { Skeleton, Stack, SxProps } from "@mui/material";
 import CallReceivedRoundedIcon from "@mui/icons-material/CallReceivedRounded";
-import Link from "next/link";
 
 type Transaction = Partial<
   Pick<
@@ -44,137 +43,133 @@ const TransactionCard = ({
   const { date, time } = useDate(new Date(createdAt ?? ""));
 
   return (
-    <Box width={"100%"}>
-      <Link href={`/my-panel/transactions/${_id}`}>
-        <Box
-          // * Box is wrapper
-          sx={(theme) => {
-            const {
-              palette: { grey, mode },
-            } = theme;
-            return {
-              p: { xs: "8px", sm: "14px" },
-              borderRadius: "12px",
-              transition: "scale 150ms ease",
-              cursor: "pointer",
-              bgcolor: muiTheme(mode, {
-                dark: grey[900],
-                light: grey[100],
-              }),
-              ":hover": {
-                scale: "1.01",
-              },
-            };
-          }}
-        >
-          {/* // * Used Stack to centeralize Type Icon with Content */}
-          <Stack flexDirection={"row"} alignItems={"center"} gap={"14px"}>
-            {/* // * Type Icon === > */}
+    <Box
+      // * Box is wrapper
+      sx={(theme) => {
+        const {
+          palette: { grey, mode },
+        } = theme;
+        return {
+          p: { xs: "8px", sm: "14px" },
+          borderRadius: "12px",
+          transition: "scale 150ms ease",
+          cursor: "pointer",
+          bgcolor: muiTheme(mode, {
+            dark: grey[900],
+            light: grey[100],
+          }),
+          ":hover": {
+            scale: "1.01",
+          },
+        };
+      }}
+    >
+      {/* // * Used Stack to centeralize Type Icon with Content */}
+      <Stack flexDirection={"row"} alignItems={"center"} gap={"14px"}>
+        {/* // * Type Icon === > */}
+        {!isPending ? (
+          <Box
+            sx={(theme) => {
+              const {
+                palette: { error, success },
+                alpha,
+              } = theme;
+              return {
+                ...shared_icon_sx,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                color: isIncome ? success.main : error.main,
+                bgcolor: isIncome
+                  ? alpha(success.main, 0.2)
+                  : alpha(error.main, 0.2),
+              };
+            }}
+          >
+            {isIncome ? (
+              <CallReceivedRoundedIcon />
+            ) : (
+              <CallReceivedRoundedIcon sx={{ rotate: "180deg" }} />
+            )}
+          </Box>
+        ) : (
+          <Skeleton variant="rounded" sx={shared_icon_sx} />
+        )}
+
+        {/* // * Transactions Content (Price) | Category =============================== > */}
+        <Box flex={"1"} minWidth={"0"}>
+          <Stack
+            justifyContent={"space-between"}
+            flexDirection={"row"}
+            alignItems={"center"}
+            flexWrap={"wrap"}
+            gap={"8px"}
+          >
+            {/* // * Category ==== > */}
+            {!isPending ? (
+              <Typography
+                fontFamily={"var(--peyda-md)"}
+                sx={shared_category_sx}
+              >
+                {category?.name ?? ""}
+              </Typography>
+            ) : (
+              <Skeleton
+                variant="text"
+                sx={shared_category_sx}
+                width={"100px"}
+              />
+            )}
+            {/* // * Amount ==== > */}
             {!isPending ? (
               <Box
+                alignSelf={{ xs: "start", _540: "center" }}
                 sx={(theme) => {
                   const {
                     palette: { error, success },
                     alpha,
                   } = theme;
                   return {
-                    ...shared_icon_sx,
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    color: isIncome ? success.main : error.main,
+                    borderRadius: "8px",
+                    p: "4px 8px",
                     bgcolor: isIncome
-                      ? alpha(success.main, 0.2)
-                      : alpha(error.main, 0.2),
+                      ? alpha(success.main, 0.1)
+                      : alpha(error.main, 0.1),
                   };
                 }}
               >
-                {isIncome ? (
-                  <CallReceivedRoundedIcon />
-                ) : (
-                  <CallReceivedRoundedIcon sx={{ rotate: "180deg" }} />
-                )}
+                <TextPrice
+                  fontSize={{
+                    xs: "14px",
+                    _540: "18px",
+                  }}
+                  price={amount}
+                  type={isIncome ? "income" : "expense"}
+                />
               </Box>
             ) : (
-              <Skeleton variant="rounded" sx={shared_icon_sx} />
+              <Skeleton
+                variant="text"
+                sx={shared_category_sx}
+                width={"100px"}
+              />
             )}
-
-            {/* // * Transactions Content (Price) | Category =============================== > */}
-            <Box flex={"1"} minWidth={"0"}>
-              <Stack
-                justifyContent={"space-between"}
-                flexDirection={"row"}
-                alignItems={"center"}
-                flexWrap={"wrap"}
-                gap={"8px"}
-              >
-                {/* // * Category ==== > */}
-                {!isPending ? (
-                  <Typography
-                    fontFamily={"var(--peyda-md)"}
-                    sx={shared_category_sx}
-                  >
-                    {category?.name ?? ""}
-                  </Typography>
-                ) : (
-                  <Skeleton
-                    variant="text"
-                    sx={shared_category_sx}
-                    width={"100px"}
-                  />
-                )}
-                {/* // * Amount ==== > */}
-                {!isPending ? (
-                  <Box
-                    alignSelf={{ xs: "start", _540: "center" }}
-                    sx={(theme) => {
-                      const {
-                        palette: { error, success },
-                        alpha,
-                      } = theme;
-                      return {
-                        borderRadius: "8px",
-                        p: "4px 8px",
-                        bgcolor: isIncome
-                          ? alpha(success.main, 0.1)
-                          : alpha(error.main, 0.1),
-                      };
-                    }}
-                  >
-                    <TextPrice
-                      fontSize={{
-                        xs: "14px",
-                        _540: "18px",
-                      }}
-                      price={amount}
-                      type={isIncome ? "income" : "expense"}
-                    />
-                  </Box>
-                ) : (
-                  <Skeleton
-                    variant="text"
-                    sx={shared_category_sx}
-                    width={"100px"}
-                  />
-                )}
-              </Stack>
-              {/* // * Date ======== > */}
-              {!isPending ? (
-                <Typography
-                  mt={"5px"}
-                  fontSize={shared_font_size}
-                >{`${date} | ${time}`}</Typography>
-              ) : (
-                <Skeleton
-                  variant="text"
-                  sx={{ fontSize: shared_font_size }}
-                  width={"150px"}
-                />
-              )}
-            </Box>
           </Stack>
+          {/* // * Date ======== > */}
+          {!isPending ? (
+            <Typography
+              mt={"5px"}
+              fontSize={shared_font_size}
+            >{`${date} | ${time}`}</Typography>
+          ) : (
+            <Skeleton
+              variant="text"
+              sx={{ fontSize: shared_font_size }}
+              width={"150px"}
+            />
+          )}
         </Box>
-      </Link>
+      </Stack>
     </Box>
   );
 };
