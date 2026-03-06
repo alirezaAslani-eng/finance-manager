2355fbbcdfceb6d12a42bccf3a0c07dce5e6e335
Alireza <alirezaaslane.mob7@gmail.com>
2026-03-06 20:38:38 +0330

    fix(style)
    removed the scroll bar of `Dialog Component`

--- packages/mui/theme/overrides/dialog.tsx
+++ packages/mui/theme/overrides/dialog.tsx
@@ -3,10 +3,11 @@ import { Components, Theme } from "@mui/material";
 const dialog = {
   defaultProps: { fullWidth: true, "aria-hidden": false },
   styleOverrides: {
-    paper: {
+    paper: ({ theme }) => ({
       borderRadius: "18px",
       backgroundImage: "unset",
-    },
+      ...(theme.custom!.noScroll as object),
+    }),
   },
 } satisfies Components<Theme>["MuiDialog"];
 
