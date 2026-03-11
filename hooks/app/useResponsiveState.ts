bd1d70d1b1cec9f77e9de57f236258ac81993a35
Alireza <alirezaaslane.mob7@gmail.com>
2026-03-11 12:04:01 +0330

    fix(type)

--- hooks/app/useResponsiveState.ts
+++ hooks/app/useResponsiveState.ts
@@ -1,9 +1,9 @@
-import { Dispatch, useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 export default function useResponsiveState<S = unknown>(
   initialState: S | (() => S),
   responsiveValue: S,
-): [S, Dispatch<S>] {
+): [S, Dispatch<SetStateAction<S>>] {
   const [value, setValue] = useState<S>(initialState);
 
   useEffect(() => {
