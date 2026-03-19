b789aa369cc47b7d01862840acb45dcdc2c0a411
Alireza <alirezaaslane.mob7@gmail.com>
2026-03-19 17:20:04 +0330

    fix(PriceInput -> props & formating issue)
    - **Props** : Let the users of this component to access the all props of `TextField` Component
    
    - **Formating issue** : The function `faNumToEnNum` allows users to enter persian numbers

--- components/ui/Input/PriceInput.tsx
+++ components/ui/Input/PriceInput.tsx
@@ -1,30 +1,24 @@
-import { useState, ChangeEvent, ChangeEventHandler, useEffect } from "react";
-import TextField from "@mui/material/TextField";
-import { useResponsiveState, useUpdateEffect } from "@/hooks";
+import { ChangeEventHandler } from "react";
+import TextField, { TextFieldProps } from "@mui/material/TextField";
+import { useResponsiveState } from "@/hooks";
+import faNumToEnNum from "@/lib/utils/app/faNumtoEnNum";
 
 const converToFaPrice = (value: number): string => {
   return value.toLocaleString();
 };
 const onlyNumber = (value: string): number => {
-  return Number(value.replace(/[^0-9]/g, ""));
+  return Number(faNumToEnNum(value).replace(/[^0-9]/g, ""));
 };
 
-interface MyProps {
+interface MyProps extends Omit<TextFieldProps, "value" | "onChange"> {
   value?: number;
   onChange?: (value: number) => void;
-  placeholder?: string;
-  label?: string;
 }
 
-export default function TomanInput({
-  onChange,
-  value,
-  placeholder = "مبلغ",
-  label,
-}: MyProps) {
+export default function PriceInput(props: MyProps) {
   const [formated, setFormated] = useResponsiveState(
     "",
-    converToFaPrice(value ?? 0),
+    converToFaPrice(props.value ?? 0),
   );
 
   const onChangeHandler: ChangeEventHandler<HTMLInputElement> = (e) => {
@@ -32,7 +26,7 @@ export default function TomanInput({
     const number = onlyNumber(input_val);
 
     // * send value to the parent ==== >
-    onChange && onChange(number);
+    props.onChange && props.onChange(number);
 
     // * set localeString ==== >
     setFormated(converToFaPrice(number));
@@ -40,11 +34,12 @@ export default function TomanInput({
 
   return (
     <TextField
-      label={label ?? placeholder}
+      label={props.label ?? props.placeholder}
+      placeholder={props.placeholder}
+      inputMode="numeric"
+      {...props}
       value={formated === "0" ? "" : formated}
       onChange={onChangeHandler}
-      placeholder={placeholder}
-      inputMode="numeric"
     />
   );
 }
