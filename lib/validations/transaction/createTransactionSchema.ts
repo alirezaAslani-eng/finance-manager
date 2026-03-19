61e0ee0d4309910872143cff820b2c569de5d060
Alireza <alirezaaslane.mob7@gmail.com>
2026-03-19 19:53:01 +0330

    fix(transaction's schema)
    we dont need to show basic error messages to the user, because the input component automaticly changes the theme into a warning color which shows the error naturaly

--- lib/validations/transaction/createTransactionSchema.ts
+++ lib/validations/transaction/createTransactionSchema.ts
@@ -2,13 +2,11 @@ import { number, object, string, enum as enum_ } from "zod";
 
 const createTransactionSchema = () => {
   return object({
-    amount: number().min(0, "مقدار تراکنش حداقل 0 میتوانه باشه"),
-    reason: string("لطفا فیلد را پر کنید")
-      .min(3, "حد اقل 3 حرف")
-      .max(500, "حد اکثر 500 حرف"),
+    amount: number(" ").min(1, " "),
+    reason: string(" ").min(3, "حد اقل 3 حرف").max(500, "حد اکثر 500 حرف"),
     type: enum_(["0", "1"]),
-    account: string().nonempty("تراکنش برای کدوم کارت بانکی هست"),
-    category: string().nonempty("تراکنش برای کدام دسته بندی هست"),
+    account: string(" ").nonempty(" "),
+    category: string(" ").nonempty(" "),
   });
 };
 
