0db316318a9c3e393d570175a2a582c757e03a30
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-12 12:25:17 +0330

    feat(restyle user-info-page)
    instead of only show a form for the user to allow them edit thier information, the new style, first shows user's activities and information like name and phone number and make some options accessable like theme button, signingout option and a link that navigates the user to another page in order to edit their information.

--- pages/my-panel/me/index.tsx
+++ pages/my-panel/me/index.tsx
@@ -1,21 +1,27 @@
-import { UserEditForm } from "@/components/module";
-import { UserCircle } from "@/components/ui";
+import {
+  UserAccessiblities,
+  UserActivities,
+  UserInfoCard,
+} from "@/components/module";
+import { FormPanelHeading } from "@/components/ui";
 import { PanelLayout } from "@/layout";
 import { PageComponent } from "@/types/page.types";
 import { Box, Container } from "@mui/material";
-import React from "react";
 
 const index: PageComponent = () => {
   return (
     <Container>
-      <Box sx={{ pt: "30px" }}>
-        {/* User's name Heading Section ==================== > */}
-        <Box sx={{ display: "flex", justifyContent: "center" }}>
-          <UserCircle />
-        </Box>
-        <Box sx={{ mt: "30px" }}>
-          <UserEditForm />
-        </Box>
+      <Box maxWidth={"600px"} mx={"auto"}>
+        <FormPanelHeading>
+          <FormPanelHeading.Title>{"تنزیمات"}</FormPanelHeading.Title>
+          <FormPanelHeading.BackButton />
+        </FormPanelHeading>
+
+        <UserInfoCard />
+
+        <UserActivities />
+
+        <UserAccessiblities />
       </Box>
     </Container>
   );
