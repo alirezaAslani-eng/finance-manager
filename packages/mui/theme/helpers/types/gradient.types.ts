1a9c61213d3231d8457f4d19bb597b3e2db0e52a
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-16 16:16:00 +0330

    fix(type)

--- packages/mui/theme/helpers/types/gradient.types.ts
+++ packages/mui/theme/helpers/types/gradient.types.ts
@@ -6,7 +6,7 @@ type GradientMethod = "linear-gradient";
 interface GradientConfig {
   gradientMethod?: GradientMethod;
   deg: `to ${Positions}` | `${number}deg`;
-  colors: CSSProperties["color"][];
+  colors: (CSSProperties["color"] | string)[];
 }
 
 export type { GradientConfig };
