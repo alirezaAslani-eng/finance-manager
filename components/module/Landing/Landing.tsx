c759d12031761cc1893ec26a78390927fd8f2cb1
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-19 22:54:37 +0330

    refactor(restyle landing component)
    second version of landing component, includes :
    - repositioned the title of landing
    - removed subtitle
    - restyled login button

--- components/module/Landing/Landing.tsx
+++ components/module/Landing/Landing.tsx
@@ -1,90 +1,76 @@
-import React from "react";
-import LandingTopBar from "../LandingTopBar/LandingTopBar";
+import { ThemeButton } from "@/components/ui";
+import { muiTheme } from "@/packages/mui";
+import LoginButton from "../Button/LoginButton";
+import { LoginRounded } from "@mui/icons-material";
+import { landingContent } from "@/constant";
 import {
   Box,
   Button,
   Container,
+  Stack,
+  Toolbar,
   Typography,
-  useMediaQuery,
-  useTheme,
+  TypographyProps,
 } from "@mui/material";
-import Link from "next/link";
-import { HeroDarkIcon, HeroIcon } from "@/assets/svg";
-import { useAuth } from "@/context";
+
+const typographyShared_sx = (({ palette }) => ({
+  color: muiTheme(palette.mode, {
+    light: palette.grey[800],
+    dark: palette.grey[50],
+  }),
+})) satisfies TypographyProps["sx"];
 
 function Landing() {
-  // * AuthContext to handle ui states ================== >
-  const { isLogin } = useAuth();
-  const isMedium = useMediaQuery("(min-width:900px)");
-  const { palette } = useTheme();
   return (
-    <Box
-      sx={{
-        height: "100svh",
-        minHeight: "700px",
-        display: "flex",
-        flexDirection: "column",
-        position: "relative",
-      }}
-    >
-      {/* TopBar ======================== > */}
-      <LandingTopBar />
-      {/* Content ============================ > */}
-      <Container
-        sx={{
-          flex: "1",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: "200px",
-          backdropFilter: "blur(35px)",
-        }}
-      >
-        {/* Right Side =================== > */}
-        <Box
-          component={"aside"}
+    <Container>
+      <Stack height={"100svh"} minHeight={"700px"} pt={"14px"}>
+        <Toolbar
+          disableGutters
           sx={{
-            textAlign: {
-              xs: "center",
-              md: "right",
-            },
+            justifyContent: "space-between",
+            alignItems: "center",
           }}
         >
-          {/* Title */}
-          <Typography
-            variant="h1"
-            component={"h3"}
-            sx={{ fontSize: { xs: "30px", sm: "35px" } }}
-          >
-            {"دیگه فراموش نکن چرا و کی واریزی داشتی یا خرج کردی"}
-          </Typography>
-
-          {/* Desc */}
-          <Typography
-            component={"p"}
-            sx={{ fontSize: { xs: "18px", md: "24px" }, mt: "20px" }}
-          >
-            {"با هزینه یار خیلی اسون و رایگان تراکنش هات را کنترل و مدریت کن"}
-          </Typography>
-
-          {/* Signin Button ----- > */}
-          <Box sx={{ mt: "35px" }}>
-            <Link href={isLogin ? "/my-panel" : "/auth/signin"}>
-              <Button size="large" variant="contained">
-                {isLogin ? `وارد پنل شوید` : "همین الان شروع کن"}
-              </Button>
-            </Link>
+          <Box flex={1} display={"flex"} justifyContent={"start"}>
+            <LoginButton sx={{ gap: "8px" }}>
+              <LoginRounded />
+              {"ورود"}
+            </LoginButton>
           </Box>
-        </Box>
-
-        {/* Left Side =========================== > */}
-        {isMedium && (
-          <Box component={"aside"} sx={{ minWidth: "min(250px,100%)" }}>
-            {palette.mode == "light" ? <HeroIcon /> : <HeroDarkIcon />}
+          <Box flex={1} display={"flex"} justifyContent={"center"}>
+            <Typography
+              fontSize={{ xs: "20px", sm: "26px" }}
+              fontFamily={"var(--peyda-md)"}
+              sx={typographyShared_sx}
+            >
+              {"هزینه یار"}
+            </Typography>
           </Box>
-        )}
-      </Container>
-    </Box>
+          <Box flex={1} display={"flex"} justifyContent={"end"}>
+            <ThemeButton />
+          </Box>
+        </Toolbar>
+
+        <Stack
+          justifyContent={"center"}
+          alignItems={"center"}
+          spacing={8}
+          flex={1}
+        >
+          <Typography
+            fontSize={{ xs: "26px", sm: "34px", md: "44px" }}
+            textAlign={"center"}
+            fontFamily={"var(--peyda-bold)"}
+            sx={typographyShared_sx}
+          >
+            {landingContent.title}
+          </Typography>
+          <Button size="large" variant="contained">
+            {landingContent.callToActionButtonContent}
+          </Button>
+        </Stack>
+      </Stack>
+    </Container>
   );
 }
 
