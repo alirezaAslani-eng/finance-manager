b182bc2b39c42036c73b57588e3dc05365364fcd
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-19 21:25:32 +0330

    fix(style)
    icon component must also renders insid the button

--- components/module/Button/LoginButton.tsx
+++ components/module/Button/LoginButton.tsx
@@ -1,31 +1,41 @@
 import { CircleLoader } from "@/components/ui";
 import { useAuth } from "@/context";
+import { identifySxProp } from "@/packages/mui";
 import { VerifiedUserOutlined } from "@mui/icons-material";
-import { Button, ButtonProps, Fade } from "@mui/material";
+import { Box, Button, ButtonProps, Fade, FadeProps } from "@mui/material";
 import Link from "next/link";
+import { useEffect } from "react";
 
+const Animationtimeout: FadeProps["timeout"] = { enter: 500 };
 function LoginButton(props: ButtonProps) {
   const { isAuthing, isLogin } = useAuth();
 
   return (
     <>
-      {isAuthing ? (
-        <CircleLoader bgcolor={"primary.main"} />
-      ) : isLogin ? (
-        <Fade in>
-          <Link href={"/my-panel"}>
-            <VerifiedUserOutlined color="primary" />
-          </Link>
-        </Fade>
-      ) : (
-        <Fade in>
-          <Link href={"/auth/signin"}>
-            <Button variant="text" {...props}>
-              {props.children}
-            </Button>
-          </Link>
-        </Fade>
-      )}
+      <Link href={isLogin ? "/my-panel" : "/auth/signin"}>
+        <Button
+          variant="text"
+          {...props}
+          sx={(tm) => ({
+            ...(isLogin ? (tm.custom?.circleButton as object) : {}),
+            ...identifySxProp(tm, props.sx),
+          })}
+        >
+          {isAuthing ? (
+            <Box>
+              <CircleLoader bgcolor={"primary.main"} />
+            </Box>
+          ) : isLogin ? (
+            <Fade in timeout={Animationtimeout}>
+              <VerifiedUserOutlined />
+            </Fade>
+          ) : (
+            <Fade in timeout={Animationtimeout}>
+              <Box>{props.children}</Box>
+            </Fade>
+          )}
+        </Button>
+      </Link>
     </>
   );
 }
